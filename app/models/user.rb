fbe8ac619c336442cc1a512e982b5357f6bf1207
Diego Gomez <diego.f.gomez.pardo@gmail.com>
2015-03-10 22:48:17 -0500

    add JWT

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,32 @@
 
 class User < ActiveRecord::Base
 
+
+	#
+	# Instance Methods
+	#
+	
+	# @return [String] whith the JWT to send the client 
+	def create_token
+		payload = {
+			user_id: self.id.to_s,
+			iat: Time.now.to_i,
+			exp: 14.days.from_now.to_i
+		}
+		JWT.encode(payload, Rails.application.secrets.secret_key_base);
+	end
+
+	#
+	# Class Methods
+	#
+
+	# @return [Boolean] true if the token is valid and false if it's not
+	def self.valid?(token)
+		begin
+		JWT.decode(token, Rails.application.secrets.secret_key_base)
+		rescue
+		false
+		end
+	end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,4 +12,26 @@ describe  User do
     it { expect(user.document_expedition_date).not_to be_nil }
     it { expect(user.phone_number).not_to be_nil }
   end
+
+  context '#instance methods' do 
+    it 'should create a JWT' do 
+      expect(subject.create_token).not_to be_nil
+    end
+  end
+
+  context '#Class methods' do 
+    before :each do 
+     @user =create :user
+     @token = @user.create_token
+    end
+
+    it 'should returns decode the JWT' do
+      expect(User.valid?(@token).first).to include("user_id" => @user.id.to_s)
+      expect(User.valid?(@token).last).to include({"typ"=>"JWT", "alg"=>"HS256"})
+    end
+
+    it 'should retrun false because the token is invalid' do 
+      expect(User.valid?("#{@token}-invalid")).to be false
+    end
+  end
 end
