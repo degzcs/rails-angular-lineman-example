00879a9e9d1e8b00359038cafc5eb7dcdb736b40
LeandroOrdonez <leandro.ordonez.ante@gmail.com>
2015-03-30 17:06:45 -0500

    add migration for appending a new column to rucoms table: 'provider_type'

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150317215700) do
+ActiveRecord::Schema.define(version: 20150329183542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20150317215700) do
     t.text     "subcontract_number"
     t.text     "mining_permit"
     t.datetime "updated_at",         default: "now()"
+    t.string   "provider_type"
   end
 
   create_table "users", force: true do |t|
