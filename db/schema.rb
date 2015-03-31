19283fc24d6de6808837dff425314e5a0e38115b
Diego Gomez <diego.f.gomez.pardo@gmail.com>
2015-03-30 22:15:13 -0500

    annotate models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150330225013) do
+ActiveRecord::Schema.define(version: 20150331024810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,17 @@ ActiveRecord::Schema.define(version: 20150330225013) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "purchases", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "provider_id"
+    t.string   "origin_certificate_sequence"
+    t.integer  "gold_batch_id"
+    t.float    "amount"
+    t.string   "origin_certificate_file"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "rucoms", primary_key: "idrucom", force: true do |t|
     t.text     "rucom_record"
--- spec/factories/rucoms.rb
+++ spec/factories/rucoms.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: rucoms
+#
+#  idrucom            :string(90)       not null, primary key
+#  rucom_record       :text
+#  name               :text
+#  status             :text
+#  mineral            :text
+#  location           :text
+#  subcontract_number :text
+#  mining_permit      :text
+#  updated_at         :datetime
+#  provider_type      :string(255)
+#  num_rucom          :string(255)
+#
+
 FactoryGirl.define do
   factory :rucom do
     idrucom "MyString"
