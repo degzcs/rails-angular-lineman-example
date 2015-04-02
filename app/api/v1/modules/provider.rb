165ab7065407bfe01a095e13cf4ce6b141452b77
jesusduardo2028@gmail.com <latharstudios@MacBook-Pro-de-Lathar.local>
2015-04-01 19:20:23 -0500

    add api provider documentation

--- app/api/v1/modules/provider.rb
+++ app/api/v1/modules/provider.rb
@@ -20,6 +20,31 @@ module V1
           requires :id, type: Integer, desc: 'User ID'
         end
 
+        params :company_info do
+          optional :company_info, type: Hash do
+            optional  :nit_number, type: String, desc: 'nit_number', documentation: { example: 'Rock' }
+            optional  :name, type: String, desc: 'name', documentation: { example: 'Rock' }
+            optional  :city, type: String, desc: 'city', documentation: { example: 'Rock' }
+            optional  :state, type: String, desc: 'state', documentation: { example: 'Rock' }
+            optional  :country , type: String, desc: 'country', documentation: { example: 'Rock' }
+            optional  :legal_representative, type: String, desc: 'legal_representative', documentation: { example: 'Rock' }
+            optional  :id_type_legal_rep, type: String, desc: 'id_type_legal_rep', documentation: { example: 'Rock' }
+            optional  :email, type: String, desc: 'email', documentation: { example: 'Rock' }
+            optional  :phone_number, type: String, desc: 'phone_number', documentation: { example: 'Rock' }
+          end
+        end
+
+        params :provider do
+          requires :provider, type: Hash do
+            optional  :rucom_id, type: Integer, desc: 'rucom_id', documentation: { example: 'Rock' }
+            optional  :document_number, type: String, desc: 'document_number', documentation: { example: 'Rock' }
+            optional  :first_name, type: String, desc: 'first_name', documentation: { example: 'Rock' }
+            optional  :last_name, type: String, desc: 'last_name', documentation: { example: 'Rock' }
+            optional  :phone_number, type: String, desc: 'phone_number', documentation: { example: 'Rock' }
+            optional  :address , type: String, desc: 'address', documentation: { example: 'Rock' }
+          end
+        end
+
       end
 
       resource :providers do
@@ -62,8 +87,8 @@ module V1
             NOTE
           }
         params do
-          requires :provider, type: Hash
-          optional :company_info , type: Hash
+          use :provider
+          use :company_info
         end
         post '/', http_codes: [
           [200, "Successful"],
@@ -91,8 +116,8 @@ module V1
           }
         params do
           requires :id
-          requires :provider, type: Hash
-          optional :company_info, type: Hash
+          use :provider
+          use :company_info
         end
         put '/', http_codes: [
           [200, "Successful"],
@@ -102,7 +127,6 @@ module V1
         ]  do
           content_type "text/json"
           provider = ::Provider.find(params[:id])
-
           provider_params = params[:provider]
           provider.company_info.update_attributes(params[:company_info]) if params[:company_info]
           provider.update_attributes(params[:provider])
