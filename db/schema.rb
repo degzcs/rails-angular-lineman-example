a3df869ba2179c0bd5a4361f0142b31630e587ca
LeandroOrdonez <leandro.ordonez.ante@gmail.com>
2015-04-02 13:11:08 -0500

    add email field to provider

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150401190653) do
+ActiveRecord::Schema.define(version: 20150402173133) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 20150401190653) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "rucom_id"
+    t.string   "email"
   end
 
   create_table "purchases", force: true do |t|
