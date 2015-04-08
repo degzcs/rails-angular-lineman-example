ea1879cd69390458e396ee7c40dcb56ef06294be
Diego Gomez <diego.f.gomez.pardo@gmail.com>
2015-04-08 14:31:22 -0500

    fix purchase test: update field name

--- db/schema.rb
+++ db/schema.rb
@@ -78,9 +78,12 @@ ActiveRecord::Schema.define(version: 20150408044007) do
 
   create_table "credit_billings", force: true do |t|
     t.integer  "user_id"
-    t.integer  "unit"
+    t.string   "unit"
     t.float    "per_unit_value"
-    t.boolean  "payment_flag",        default: false
+    t.float    "iva_value"
+    t.float    "discount"
+    t.float    "total_amount"
+    t.boolean  "payment_flag"
     t.datetime "payment_date"
     t.float    "discount_percentage"
     t.datetime "created_at"
@@ -143,7 +146,7 @@ ActiveRecord::Schema.define(version: 20150408044007) do
   end
 
   create_table "rucoms", force: true do |t|
-    t.string   "idrucom",            limit: 90,                   null: false
+    t.string   "idrucom",            limit: 90,                                 null: false
     t.text     "rucom_record"
     t.text     "name"
     t.text     "status"
@@ -151,7 +154,7 @@ ActiveRecord::Schema.define(version: 20150408044007) do
     t.text     "location"
     t.text     "subcontract_number"
     t.text     "mining_permit"
-    t.datetime "updated_at",                    default: "now()"
+    t.datetime "updated_at",                    default: '2015-04-08 03:40:49'
     t.string   "provider_type"
     t.string   "num_rucom"
   end
