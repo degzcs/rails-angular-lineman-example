5ba8fcaa142b0648e1ee8c2b03d597f716719d16
jesusduardo2028@gmail.com <latharstudios@MacBook-Pro-de-Lathar.local>
2015-04-07 20:12:06 -0500

    modify new ip of staging server

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,9 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{deploy@104.211.32.220}
-role :web, %w{deploy@104.211.32.220}
-role :db,  %w{deploy@104.211.32.220}
+role :app, %w{deploy@104.211.35.165}
+role :web, %w{deploy@104.211.35.165}
+role :db,  %w{deploy@104.211.35.165}
 set :branch, 'staging'
 
 
@@ -16,7 +16,7 @@ set :branch, 'staging'
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server '104.211.32.220', user: 'deploy', roles: %w{web app db}, my_property: :my_value
+server '104.211.35.165', user: 'deploy', roles: %w{web app db}, my_property: :my_value
 
 
 # Custom SSH Options
