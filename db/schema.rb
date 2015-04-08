64474b5a7196612c551900032b7fd91dc89c9f7f
Javier Suarez <luisjavier.suarezmeza@gmail.com>
2015-04-08 09:59:40 -0500

    run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -121,11 +121,11 @@ ActiveRecord::Schema.define(version: 20150408044007) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "rucom_id"
-    t.string   "email"
     t.string   "identification_number_file"
     t.string   "rut_file"
     t.string   "mining_register_file"
     t.string   "photo_file"
+    t.string   "email"
     t.integer  "population_center_id"
   end
 
@@ -151,7 +151,7 @@ ActiveRecord::Schema.define(version: 20150408044007) do
     t.text     "location"
     t.text     "subcontract_number"
     t.text     "mining_permit"
-    t.datetime "updated_at",                    default: '2015-04-04 15:57:55'
+    t.datetime "updated_at",                    default: "now()"
     t.string   "provider_type"
     t.string   "num_rucom"
   end
