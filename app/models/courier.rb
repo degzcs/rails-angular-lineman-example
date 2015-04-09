a251e1ffbdbe35b315f81b2357f3d113207514d0
LeandroOrdonez <leandro.ordonez.ante@gmail.com>
2015-04-09 18:18:38 -0500

    add field validation in courier model

--- app/models/courier.rb
+++ app/models/courier.rb
@@ -16,4 +16,11 @@
 #
 
 class Courier < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone_number, presence: true
+  validates :address, presence: true
+  validates :id_document_type, presence: true
+  validates :id_document_number, presence: true
+
 end
