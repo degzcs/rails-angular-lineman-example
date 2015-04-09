fcafcd8b328c38cced0e9fa442297877b2fc0890
LeandroOrdonez <leandro.ordonez.ante@gmail.com>
2015-04-09 15:15:56 -0500

    modify schema

--- db/schema.rb
+++ db/schema.rb
@@ -78,12 +78,9 @@ ActiveRecord::Schema.define(version: 20150409072033) do
 
   create_table "credit_billings", force: true do |t|
     t.integer  "user_id"
-    t.string   "unit"
+    t.integer  "unit"
     t.float    "per_unit_value"
-    t.float    "iva_value"
-    t.float    "discount"
-    t.float    "total_amount"
-    t.boolean  "payment_flag"
+    t.boolean  "payment_flag",        default: false
     t.datetime "payment_date"
     t.float    "discount_percentage", default: 0.0,   null: false
     t.datetime "created_at"
@@ -126,11 +123,11 @@ ActiveRecord::Schema.define(version: 20150409072033) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "rucom_id"
+    t.string   "email"
     t.string   "identification_number_file"
     t.string   "rut_file"
     t.string   "mining_register_file"
     t.string   "photo_file"
-    t.string   "email"
     t.integer  "population_center_id"
   end
 
@@ -148,7 +145,7 @@ ActiveRecord::Schema.define(version: 20150409072033) do
   end
 
   create_table "rucoms", force: true do |t|
-    t.string   "idrucom",            limit: 90,                                 null: false
+    t.string   "idrucom",            limit: 90,                   null: false
     t.text     "rucom_record"
     t.text     "name"
     t.text     "status"
@@ -156,7 +153,7 @@ ActiveRecord::Schema.define(version: 20150409072033) do
     t.text     "location"
     t.text     "subcontract_number"
     t.text     "mining_permit"
-    t.datetime "updated_at",                    default: '2015-04-08 03:40:49'
+    t.datetime "updated_at",                    default: "now()"
     t.string   "provider_type"
     t.string   "num_rucom"
   end
