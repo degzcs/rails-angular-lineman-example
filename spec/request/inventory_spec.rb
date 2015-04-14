f3560be3177c207e6a1e696a31b6f69214b11f47
jesusduardo2028@gmail.com <latharstudios@MacBook-Pro-de-Lathar.local>
2015-04-14 16:50:13 -0500

    fix inventory spec

--- spec/request/inventory_spec.rb
+++ spec/request/inventory_spec.rb
@@ -11,10 +11,11 @@ RSpec.describe "Inventory:", :type => :request do
     before(:all) do
       @user2 = create(:user)
       gold_batch = create(:gold_batch)
+      provider = create(:provider)
       @number_of_purchases_user_1 = 10
       @number_of_purchases_user_2 = 5
-      create_list(:purchase, @number_of_purchases_user_1, user_id: @user.id, gold_batch_id: gold_batch.id)
-      create_list(:purchase, @number_of_purchases_user_2, user_id: @user2.id, gold_batch_id: gold_batch.id)
+      create_list(:purchase, @number_of_purchases_user_1, user_id: @user.id, gold_batch_id: gold_batch.id, provider_id: provider.id)
+      create_list(:purchase, @number_of_purchases_user_2, user_id: @user2.id, gold_batch_id: gold_batch.id,provider_id: provider.id)
     end
     it "responds with a paginated list of the current user purchases" do
       get '/api/v1/purchases', { per_page: 40 } , { "Authorization" => "Barer #{@token}" }
