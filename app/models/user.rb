7eea587577e23355c520026e7b748e20aa3e6777
Diego Gomez <diego.f.gomez.pardo@gmail.com>
2015-04-26 19:05:04 -0500

    set buyer city as default in all origin certificates

--- app/models/user.rb
+++ app/models/user.rb
@@ -64,6 +64,7 @@ class User < ActiveRecord::Base
 	end
 
 	# NOTE: what is Client class for?... I think is better use a Single-Table Inheritance approach here!
+	# NOTE2: Also this client is created but without a implicit relation/association with the current user
 	  def save_client
 
 			client_hash = { "first_name" => self.first_name,
