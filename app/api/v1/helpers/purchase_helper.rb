24dd1a52ac4e05e234cc76340c3f8d1dde18cfcc
Diego Gomez <diego.f.gomez.pardo@gmail.com>
2015-05-03 02:00:34 -0500

    set trazoro field in purchases table

--- app/api/v1/helpers/purchase_helper.rb
+++ app/api/v1/helpers/purchase_helper.rb
@@ -5,11 +5,17 @@ module V1
 
         def format_params(params)
           params.tap do
+            trazoro = false
             files =params[:purchase].slice(:files)[:files]
             origin_certificate_file = files.reject{|file| file['filename'] =~ /seller_picture/}.first
             seller_picture = files.select{|file| file['filename'] =~ /seller_picture/}.first
-            origin_certificate_file = Sale.find( params[:purchase][:sale_id] ).origin_certificate_file if origin_certificate_file.nil? && params[:purchase][:sale_id].present?
-            params[:purchase].except!(:files).merge!(origin_certificate_file: origin_certificate_file, seller_picture: seller_picture)
+
+            if origin_certificate_file.nil? && params[:purchase][:sale_id].present?
+              origin_certificate_file = Sale.find( params[:purchase][:sale_id] ).origin_certificate_file
+              trazoro = true
+            end
+
+            params[:purchase].except!(:files).merge!(origin_certificate_file: origin_certificate_file, seller_picture: seller_picture, trazoro: trazoro)
           end
         end
 
