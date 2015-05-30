34ee7429ec247288d7942f663fc1fcb0367e1fbb
Diego Gomez <diego.f.gomez.pardo@gmail.com>
2015-05-30 14:48:26 -0500

    remove validation for udpated_at field in rucom spec

--- spec/models/rucom_spec.rb
+++ spec/models/rucom_spec.rb
@@ -21,7 +21,7 @@
 require 'spec_helper'
 
 describe Rucom  do
-  
+
   context "test factory" do
     let(:rucom) {build(:rucom)}
     it {expect(rucom.idrucom).not_to be_nil }
@@ -32,7 +32,6 @@ describe Rucom  do
     it {expect(rucom.location).not_to be_nil }
     it {expect(rucom.subcontract_number).not_to be_nil}
     it {expect(rucom.mining_permit).not_to be_nil}
-    it {expect(rucom.updated_at).not_to be_nil}
     it {expect(rucom.provider_type).not_to be_nil }
     it {expect(rucom.num_rucom).not_to be_nil}
   end
