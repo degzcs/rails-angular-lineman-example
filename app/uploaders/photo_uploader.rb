9a66acef5c709ae9a7f9cc17eccb4f69191c9ee2
jesusduardo2028@gmail.com <latharstudios@MacBook-Pro-de-Lathar.local>
2015-06-05 09:21:31 -0500

    resize thumb on photo uploader

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -31,10 +31,10 @@ class PhotoUploader < CarrierWave::Uploader::Base
   #   # do something
   # end
 
-  # Create different versions of your uploaded files:
-  version :thumb do
-    process :resize_to_fit => [50, 50]
-  end
+  # # Create different versions of your uploaded files:
+  # version :thumb do
+  #   process :resize_to_fit => [50, 50]
+  # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
@@ -42,9 +42,9 @@ class PhotoUploader < CarrierWave::Uploader::Base
     %w(jpg jpeg png pdf svg)
   end
 
-  # version :thumb do
-  #   process resize_to_fill: [200,200]
-  # end
+  version :thumb do
+    process resize_to_fill: [200,200]
+  end
 
 
   # Override the filename of the uploaded files:
