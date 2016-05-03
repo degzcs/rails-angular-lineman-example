aef45317052fcb7b9747cd4e8c88232da3dfdd98
Diego Gomez <diego.f.gomez.pardo@gmail.com>
2016-05-03 12:16:35 -0500

    add environment configuration for carrierwave: use AWS 3 in production and staging and file strategy in local and test

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,31 @@
-if Rails.env.test? or Rails.env.cucumber?
+if Rails.env.production? || Rails.env.staging?
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'                        # required
+    config.fog_credentials = {
+      provider:              'AWS',                        # required
+      aws_access_key_id:     APP_CONFIG[:AWS_S3_ACCESS_KEY_ID],                        # required
+      aws_secret_access_key: APP_CONFIG[:AWS_S3_SECRET_ACCESS_KEY],                        # required
+      region:                APP_CONFIG[:AWS_S3_REGION],
+      # host:                  's3.example.com',             # optional, defaults to nil
+      # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
+    }
+    config.fog_directory  = APP_CONFIG[:AWS_S3_BUCKET_NAME]                          # required
+    config.fog_public     = false                                        # optional, defaults to true
+    config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
+  end
+elsif Rails.env.test? || Rails.env.cucumber?
   CarrierWave.configure do |config|
     config.storage = :file
     config.enable_processing = false
   end
+
   class PdfUploader < CarrierWave::Uploader::Base
     storage :file
     def store_dir
       "#{Rails.root}/spec/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
     end
   end
-  
+
   class PhotoUploader < CarrierWave::Uploader::Base
     storage :file
 
@@ -18,8 +34,8 @@ if Rails.env.test? or Rails.env.cucumber?
     end
   end
 
-  CarrierWave.configure do |config|
-    config.base_path = ''
-  end
+  # CarrierWave.configure do |config|
+  #   config.base_path = ''
+  # end
 
 end
