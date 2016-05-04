e7d7c0903f9c885884ae32ba5eebc03210ad7f6c
Diego Gomez <diego.f.gomez.pardo@gmail.com>
2016-05-04 09:22:38 -0500

    imporove seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,65 +1,73 @@
   # TODO: use the DANE xls file to import thsi states and cities
-  puts 'Creating population centers ...'
+puts 'Creating population centers ...'
 
-    country = Country.find_or_create_by(name: 'Colombia')
-    state = State.find_or_create_by(name: 'Antíoquia', code: '05', country: country)
-    city = City.find_or_create_by(name: 'Medellín', state: state, code: '001' )
+  country = Country.find_or_create_by(name: 'Colombia')
+  state = State.find_or_create_by(name: 'Antíoquia', code: '05', country: country)
+  city = City.find_or_create_by(name: 'Medellín', state: state, code: '001' )
 
-    population_center = PopulationCenter.find_or_create_by(name: 'Distrito de la innovación', city: city, code: '000')
-  begin
-    # TODO: add the real files to this company (rut, nit, etc.)
-    company = FactoryGirl.create(:company,
-                                  name: 'Trazoro',
-                                  city: city.name,
-                                  state: state.name,
-                                  country: country.name,
-                                  legal_representative: 'Diego Caicedo',
-                                  id_type_legal_rep: 'NIT',
-                                  id_number_legal_rep: '123456789',
-                                  nit_number: '123456789',
-                                  email: 'soport@trazoro.co',
-                                  phone_number: '3004322618',
-                                )
-  rescue
-    company = Company.find_by(nit_number: '123456789')
-    puts 'Company already created!'
-  end
+  population_center = PopulationCenter.find_or_create_by(name: 'Distrito de la innovación', city: city, code: '000')
+begin
+  # TODO: add the real files to this company (rut, nit, etc.)
+  company = FactoryGirl.create(:company,
+                                name: 'Trazoro',
+                                city: city.name,
+                                state: state.name,
+                                country: country.name,
+                                legal_representative: 'Diego Caicedo',
+                                id_type_legal_rep: 'NIT',
+                                id_number_legal_rep: '123456789',
+                                nit_number: '123456789',
+                                email: 'soport@trazoro.co',
+                                phone_number: '3004322618',
+                              )
+rescue
+  company = Company.find_by(nit_number: '123456789')
+  puts 'There was something wrong!'
+end
 
+begin
   office = company.offices.first
+rescue => e
+  puts 'There is not Office or the Campany was no created properly'
+end
 
-  puts 'Creating basic users ...'
-  begin
-    AdminUser.create(email:'soporte@trazoro.co',password: 'A7l(?/]03tal9-%g4', password_confirmation: 'A7l(?/]03tal9-%g4')
+puts 'Creating basic users ...'
+begin
+  AdminUser.create(email:'soporte@trazoro.co',password: 'A7l(?/]03tal9-%g4', password_confirmation: 'A7l(?/]03tal9-%g4')
+  FactoryGirl.create(:user,
+        first_name: 'Tech',
+        last_name: 'Trazoro',
+        email: 'tech@trazoro.co',
+        password: 'A7l(?/]03tal9-%g4',
+        password_confirmation: 'A7l(?/]03tal9-%g4' ,
+        population_center: population_center,
+        office: office,)
 
-    FactoryGirl.create(:user,
-          first_name: 'Tech',
-          last_name: 'Trazoro',
-          email: 'tech@trazoro.co',
-          password: 'A7l(?/]03tal9-%g4',
-          password_confirmation: 'A7l(?/]03tal9-%g4' ,
-          population_center: population_center,
-          office: office,)
+  FactoryGirl.create(:user,
+        first_name: 'Diego',
+        last_name: 'Gomez',
+        email: 'diego.gomez@trazoro.co',
+        password: 'A7l(?/]03tal9-%g4',
+        password_confirmation: 'A7l(?/]03tal9-%g4' ,
+        population_center: population_center,
+        office: office,)
 
-    FactoryGirl.create(:user,
-          first_name: 'Diego',
-          last_name: 'Gomez',
-          email: 'diego.gomez@trazoro.co',
-          password: 'A7l(?/]03tal9-%g4',
-          password_confirmation: 'A7l(?/]03tal9-%g4' ,
-          population_center: population_center,
-          office: office,)
+  FactoryGirl.create(:user,
+        first_name: 'Jesus',
+        last_name: 'Munoz',
+        email: 'jesus.munoz@trazoro.co',
+        password: 'A7l(?/]03tal9-%g4',
+        password_confirmation: 'A7l(?/]03tal9-%g4',
+        population_center: population_center,
+        office: office,)
+rescue => e
+  puts 'There is something wrong!!!, perhaps the users were already created.'
+end
 
-    FactoryGirl.create(:user,
-          first_name: 'Jesus',
-          last_name: 'Munoz',
-          email: 'jesus.munoz@trazoro.co',
-          password: 'A7l(?/]03tal9-%g4',
-          password_confirmation: 'A7l(?/]03tal9-%g4',
-          population_center: population_center,
-          office: office,)
-  rescue
-    puts 'There is something wrong!!!'
-  end
+puts 'Associating trazoro users ...'
+# Add to the trazoro Office
+trazoro_users = User.where(email: ['diego.gomez@trazoro.co', 'jesus.munoz@trazoro.co', 'tech@trazoro.co'])
+trazoro_users.update_all(office_id: office.id)
 
 unless Rails.env.production?
   # Rucoms for User as clients
@@ -84,25 +92,26 @@ unless Rails.env.production?
   # end
 
   if ENV['FAKE_INFO'] == 'yes'
-    puts 'creating rucoms ...'
+    puts 'Adding fake information ...'
+    puts 'Creating rucoms ...'
     rucoms = FactoryGirl.create_list(:rucom, 10)
 
-    puts 'creating extenal_users ...'
+    puts 'Creating extenal_users ...'
     rucoms.each do|rucom|
       FactoryGirl.create(:external_user,personal_rucom: rucom, population_center: population_center, available_credits: rand(2000 .. 50000)) #, population_center_id: population_center.id)
     end
 
-    puts 'creating barequeros and chatarreros providers ...'
+    puts 'Creating barequeros and chatarreros providers ...'
     bc_rucoms = FactoryGirl.create_list(:rucom, 10, provider_type: ["Barequero", "Chatarrero"].sample)
     bc_rucoms.each do|rucom|
       FactoryGirl.create(:external_user,personal_rucom: rucom, population_center_id: PopulationCenter.first.id, available_credits: rand(2000 .. 50000))
     end
 
-    puts 'creating pawnshops providers ...'
+    puts 'Creating pawnshops providers ...'
     ccv_rucoms = FactoryGirl.create_list(:rucom, 10, provider_type: 'Casa de Compraventa')
     ccv_rucoms.each do|rucom|
       FactoryGirl.create(:external_user,personal_rucom: rucom, population_center_id: PopulationCenter.first.id, available_credits: rand(2000 .. 50000))
     end
   end
-
+  puts 'Done!'
 end
