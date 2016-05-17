900b06a903a420b27c8c2c32519b840b5cc8eec8
Diego Gomez <diego.f.gomez.pardo@gmail.com>
2016-05-17 11:40:34 -0500

    add helper methods to handle the gold_batch extra_info field

--- app/models/gold_batch.rb
+++ app/models/gold_batch.rb
@@ -8,12 +8,81 @@
 #  inventory_id :integer
 #  created_at   :datetime
 #  updated_at   :datetime
+#  extra_info   :text
 #
 
+# NOTE: the fine_grams are the resulting from grams*grade/999 (999 is the maximun gold purity percentage)
+# the plataform make transaction with the fine grams, but in the extra_info you will be able to
+# find where this value come from. See the measurement_converter_service.coffee file for more information
+
 class GoldBatch < ActiveRecord::Base
+
+  #
+  # Serialized field
+  # Example,
+  # {
+  #  grams: 7,
+  #  castellanos: 5,
+  #  tomines: 8,
+  #  reales: 10,
+  #  onzas: 14,
+  # }
+
+  serialize :extra_info
+
   #
   # Associations
   #
+
   has_many :purchases
   has_many :sales
+
+  #
+  # Instance Methods
+  #
+
+  def extra_data
+    OpenStruct.new(extra_info)
+  end
+
+  def grams
+    extra_data.grams
+  end
+
+  def castellanos
+    extra_data.castellanos
+  end
+
+  def tomines
+    extra_data.tomines
+  end
+
+  def onzas
+    extra_data.onzas
+  end
+
+  def reales
+    extra_data.reales
+  end
+
+  def grams?
+    grams.present? && !grams.zero?
+  end
+
+  def castellanos?
+    castellanos.present? && !castellanos.zero?
+  end
+
+  def tomines?
+    tomines.present? && !tomines.zero?
+  end
+
+  def onzas?
+    onzas.present? && !onzas.zero?
+  end
+
+  def reales?
+    reales.present? && !reales.zero?
+  end
+
 end
