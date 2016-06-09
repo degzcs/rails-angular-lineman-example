647b6bac4e3555a0a627d66ee79a0528edc94231
Diego Gomez <diego.f.gomez.pardo@gmail.com>
2016-06-08 20:07:15 -0500

    fix puchase associon with documents

--- spec/models/sold_batch_spec.rb
+++ spec/models/sold_batch_spec.rb
@@ -14,20 +14,20 @@ require 'spec_helper'
 
 describe SoldBatch, type: :model do
   context "test factory" do
-    let(:sold_batch) {build(:sold_batch)}
+    let(:sold_batch) { build(:sold_batch) }
     it {expect(sold_batch.purchase_id).not_to be_nil }
     it {expect(sold_batch.grams_picked).not_to be_nil }
     it {expect(sold_batch.sale_id).not_to be_nil}
   end
   context "test creation" do
-    it "should create a new sale with valid data" do
+    it "should build a new sale with valid data" do
       expect(build(:sold_batch)).to be_valid
     end
 
-    it "should not allow to create a sold_batch without a purchase id " do
+    it "should not allow to build a sold_batch without a purchase id " do
       expect(build(:sold_batch, purchase_id: nil)).not_to be_valid
     end
-    it "should not allow to create a sold_batch without grams picked" do
+    it "should not allow to build a sold_batch without grams picked" do
       expect(build(:sold_batch, grams_picked: nil)).not_to be_valid
     end
   end
