cf07618e2acd3d1ff21f86b3e273bfa1a0468a60
Santiago lopez <santilopez114@gmail.com>
2016-07-01 16:53:32 -0500

    annotate profile model and spec

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: profiles
+#
+#  id                        :integer          not null, primary key
+#  document_number           :string(255)
+#  phone_number              :string(255)
+#  avaible_credits           :float
+#  address                   :string(255)
+#  rut_file                  :string(255)
+#  photo_file                :string(255)
+#  mining_authorization_file :text
+#  legal_representative      :boolean
+#  id_document_file          :text
+#  nit_number                :integer
+#  city_id                   :integer
+#  created_at                :datetime
+#  updated_at                :datetime
+#
+
 class Profile < ActiveRecord::Base
   belongs_to :city
+
+
 end
--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: profiles
+#
+#  id                        :integer          not null, primary key
+#  document_number           :string(255)
+#  phone_number              :string(255)
+#  avaible_credits           :float
+#  address                   :string(255)
+#  rut_file                  :string(255)
+#  photo_file                :string(255)
+#  mining_authorization_file :text
+#  legal_representative      :boolean
+#  id_document_file          :text
+#  nit_number                :integer
+#  city_id                   :integer
+#  created_at                :datetime
+#  updated_at                :datetime
+#
+
 require 'spec_helper'
 
 describe Profile, type: :model do
