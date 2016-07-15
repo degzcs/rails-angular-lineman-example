58606c92294271f382097588e3dbfc777659dcbd
Santiago lopez <santilopez114@gmail.com>
2016-07-15 17:59:57 -0500

    improve service for generate Origin Certificates by using Presenters

--- app/services/origin_certificates/draw_authorized_provider_origin_certificate.rb
+++ app/services/origin_certificates/draw_authorized_provider_origin_certificate.rb
@@ -10,14 +10,16 @@ class OriginCertificates::DrawAuthorizedProviderOriginCertificate < Prawn::Docum
   def call(options={})
     raise 'You must to provide a purchase option' if options[:purchase].blank?
     raise 'You must to provide a signature_picture option' if options[:signature_picture].blank?
-    purchase = options[:purchase]
+    raise 'You must to provide a purchase option' if options[:date].blank?
+    purchase_presenter = PurchasePresenter.new(options[:purchase], nil)
+    date_to_day = options[:date]
     signature_picture = options[:signature_picture]
     begin
-      generate_certificate(purchase, signature_picture)
+      generate_certificate(purchase_presenter, signature_picture, date_to_day)
       @response[:success] = true
-    rescue => e
+    rescue => exception
       @response[:success] = false
-      @response[:errors] = e
+      @response[:errors] << exception.message
     end
     @response
   end
@@ -28,25 +30,27 @@ class OriginCertificates::DrawAuthorizedProviderOriginCertificate < Prawn::Docum
   end
 
   # Generar certificado para barequeros y chatarreros
-  def generate_certificate(purchase, signature_picture)
-    date = Date.today
+  def generate_certificate(purchase_presenter, signature_picture, date_to_day)
     file = File.open(File.join(Rails.root, 'vendor','pdfs','formato_certificado_origen_barequero_chatarrero.pdf'))
     start_new_page({:template => "#{file.path}" , :template_page => 1})
 
     # complete information
 
     # header
+    buyer_presenter = purchase_presenter.buyer_presenter
+
     move_cursor_to 590
-    text_box purchase.user.office.city.name, :at => [370,cursor], :width => 250, :height => 15 , :overflow => :shrink_to_fit
+    text_box buyer_presenter.city_name, :at => [370,cursor], :width => 250, :height => 15 , :overflow => :shrink_to_fit
     move_cursor_to 586
 
-    text_box "#{date.day}", :at => [141,cursor] , :width => 85
-    text_box "#{date.month}", :at => [178,cursor] , :width => 85
-    text_box "#{date.year}" , :at => [222,cursor], :width => 85
+    text_box "#{date_to_day.day}", :at => [141,cursor] , :width => 85
+    text_box "#{date_to_day.month}", :at => [178,cursor] , :width => 85
+    text_box "#{date_to_day.year}" , :at => [222,cursor], :width => 85
 
      #body producer
+    seller_presenter =  purchase_presenter.seller_presenter
     move_cursor_to 532
-    case   purchase.seller.rucom.provider_type.downcase
+    case   seller_presenter.provider_type.downcase
       when 'barequero'
         text_box "X", :at => [386,cursor] , :width => 85
       when 'chatarrero'
@@ -55,35 +59,35 @@ class OriginCertificates::DrawAuthorizedProviderOriginCertificate < Prawn::Docum
         puts 'No se ha enviado  un parametro o este es incorrecto'
     end
 
-    purchase_seller_profile_name = purchase.seller.profile.first_name + " " +  purchase.seller.profile.last_name
-
+    # Seller
     move_cursor_to 492
-    text_box purchase_seller_profile_name , :at => [300,cursor] , :width => 300, :height => 15 , :overflow => :shrink_to_fit
+    text_box seller_presenter.name , :at => [300,cursor] , :width => 300, :height => 15 , :overflow => :shrink_to_fit
 
     move_cursor_to 462
-    text_box purchase.seller.profile.document_number , :at => [300,cursor] , :width => 300, :height => 15 , :overflow => :shrink_to_fit
+    text_box seller_presenter.document_number , :at => [300,cursor] , :width => 300, :height => 15 , :overflow => :shrink_to_fit
 
     move_cursor_to 422
-    text_box purchase.seller.profile.city.state.name , :at => [300,cursor] , :width => 300, :height => 15 , :overflow => :shrink_to_fit
+    text_box seller_presenter.state_name , :at => [300,cursor] , :width => 300, :height => 15 , :overflow => :shrink_to_fit
 
     move_cursor_to 362
-    text_box purchase.seller.profile.city.name , :at => [300,cursor] , :width => 300, :height => 15 , :overflow => :shrink_to_fit
+    text_box seller_presenter.city_name , :at => [300,cursor] , :width => 300, :height => 15 , :overflow => :shrink_to_fit
 
-
+    # Gold Batch
+    gold_batch_presenter = purchase_presenter.gold_batch_presenter
     move_cursor_to 322
     text_box "Oro" , :at => [300,cursor] , :width => 300, :height => 15 , :overflow => :shrink_to_fit
 
 
      move_cursor_to 294
-     text_box purchase.gold_batch.fine_grams.to_s , :at => [300,cursor] , :width => 300, :height => 15 , :overflow => :shrink_to_fit
+     text_box gold_batch_presenter.formatted_fine_grams , :at => [300,cursor] , :width => 300, :height => 15 , :overflow => :shrink_to_fit
 
     move_cursor_to 267
     text_box "Gramos" , :at => [350,cursor] , :width => 300, :height => 15 , :overflow => :shrink_to_fit
 
-    #body buyer
+    #Buyer
 
     move_cursor_to 219
-    text_box purchase.user.company.name , :at => [300,cursor] , :width => 300, :height => 15 , :overflow => :shrink_to_fit
+    text_box buyer_presenter.company_name , :at => [300,cursor] , :width => 300, :height => 15 , :overflow => :shrink_to_fit
 
     move_cursor_to 185
     case "nit".downcase
@@ -99,10 +103,10 @@ class OriginCertificates::DrawAuthorizedProviderOriginCertificate < Prawn::Docum
     end
 
     move_cursor_to 149
-    text_box purchase.user.company.nit_number , :at => [300,cursor] , :width => 300, :height => 15 , :overflow => :shrink_to_fit
+    text_box buyer_presenter.nit_number , :at => [300,cursor] , :width => 300, :height => 15 , :overflow => :shrink_to_fit
 
     move_cursor_to 112
-    text_box purchase.user.company.rucom.num_rucom , :at => [350,cursor] , :width => 300, :height => 15 , :overflow => :shrink_to_fit
+    text_box buyer_presenter.rucom_number , :at => [350,cursor] , :width => 300, :height => 15 , :overflow => :shrink_to_fit
     move_cursor_to 86
     signature = image(signature_picture, :at => [280,cursor], :fit => [200,100])
   end
