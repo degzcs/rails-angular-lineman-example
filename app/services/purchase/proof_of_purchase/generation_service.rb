d0421c3603ced3511b544d6b3899bea4c31e932f
Santiago lopez <santilopez114@gmail.com>
2016-07-15 18:05:47 -0500

    change the way to the response of the generation service

--- app/services/purchase/proof_of_purchase/generation_service.rb
+++ app/services/purchase/proof_of_purchase/generation_service.rb
@@ -2,16 +2,17 @@ class Purchase::ProofOfPurchase::GenerationService
 
   attr_accessor :purchase_presenter, :draw_pdf_service_class
 
-  def intiliaze(options={})
+  def initialize(options={})
+    @response = {}
+    @response[:errors] = []
   end
 
   # @return [ Hash ] with the success or errors
   def call(options={})
     raise "You must to provide a purchase param" if options[:purchase].blank?
-    @purchase_presenter = ProofOfPurchasePresenter.new(options[:purchase], nil)
+    @purchase_presenter = PurchasePresenter.new(options[:purchase], nil)
     @draw_pdf_service_class = options[:draw_pdf_service] || ::Purchase::ProofOfPurchase::DrawPDF
     timestamp = options[:timestamp] || Time.now.to_i
-    response = {}
 
     temporal_folder_path = "#{ Rails.root }/tmp/purchases/#{timestamp}"
     document_type = 'equivalent_document' # TODO: update this when it is made a invoice instead equiv_doc.
@@ -19,12 +20,12 @@ class Purchase::ProofOfPurchase::GenerationService
 
     if create_temporal_folder(temporal_folder_path)
       purchase_presenter = generate_file_for(@purchase_presenter, temporal_file_path, document_type)
-      response[:success] = purchase_presenter.save!
-      response[:errors] = purchase_presenter.errors.full_messages
+      @response[:success] = purchase_presenter.save!
+      @response[:errors] << purchase_presenter.errors.full_messages
     else
       raise 'The folder was not creted!'
     end
-    response
+    @response
   end
 
   private
