6e78d209f23ff72f717eab3e89a7ccae558eeebb
Paulo Carmona <pcarmonaz@hotmail.com>
2016-07-21 18:13:54 -0500

    Add tests of Formater Service and fix the service

--- lib/rucom_services/formater.rb
+++ lib/rucom_services/formater.rb
@@ -4,22 +4,42 @@ module RucomServices
     FIELDS_TRADERS = %i(rucom_number name minerals state)
     FIELDS_AUTORIZED_PROV = %i(name minerals localization)
 
+    attr_accessor :response
+
     def initialize
       self.response = {}
       self.response[:errors] = []
     end  
 
-    def call(options ={})
-      @data = options[:data]
-      fields = (options[:format] == :trader) ? FIELDS_TRADERS : FIELDS_AUTORIZED_PROV
-      format!(fields)
+    def call(options ={})      
+      if options[:data] && options[:format]
+        @data = options[:data] 
+        format!(assign_fields(options))
+      else  
+        @response[:errors] << 
+          "call: No was send data as parameter when invoqued the method." unless options[:data]
+        @response[:errors] << 
+          "call: No was send the format Key when invoqued the method." unless options[:format]
+        @response
+      #fields = (options[:format] == :trader) ? FIELDS_TRADERS : FIELDS_AUTORIZED_PROV
+      end
     end  
+    
+    def assign_fields(options)
+      if options[:format] == :trader_response
+        FIELDS_TRADERS
+      elsif options[:format] == :autorized_provider_response
+        FIELDS_AUTORIZED_PROV
+      else
+        nil
+      end
+    end
     
     def format!(field_names)
       field_names.each_with_index do |key, index|
-        response[key] = @data[index].content
+        @response[key] = @data[index].content
       end
-      response
+      @response
     end
   end
 end  
