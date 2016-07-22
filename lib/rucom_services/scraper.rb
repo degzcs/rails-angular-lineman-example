e071fbcc087968dff73a2922224f0e40c77703d4
Paulo Carmona <pcarmonaz@hotmail.com>
2016-07-22 17:20:27 -0500

    Apply the changes whith the setting attribute object in the Scraper Service and add news test

--- lib/rucom_services/scraper.rb
+++ lib/rucom_services/scraper.rb
@@ -1,10 +1,10 @@
 #  RucomServices Module Allows to handle all the service to extract the data from de Rucom page.
 module RucomServices
   class Scraper
-    PAGE_URL = 'http://tramites.anm.gov.co:8080/Portal/pages/consultaListados/anonimoListados.jsf'
-    HIDDEN_ELEMENTS_ID = {step_0: 'form:trol_panel', step_1: 'form:tIdentificacion_panel', step_2: 'form:numId'}
-    SELECT_OPTIONS = %w(Barequero CEDULA NIT)
-    TABLE_BODY_ID = "form:tablaListadosANM_data" # "//*[@id='form:tablaListadosANM_data']"
+    # PAGE_URL = 'http://tramites.anm.gov.co:8080/Portal/pages/consultaListados/anonimoListados.jsf'
+    # HIDDEN_ELEMENTS_ID = {step_0: 'form:trol_panel', step_1: 'form:tIdentificacion_panel', step_2: 'form:numId'}
+    # SELECT_OPTIONS = %w(Barequero CEDULA NIT)
+    # TABLE_BODY_ID = "form:tablaListadosANM_data" # "//*[@id='form:tablaListadosANM_data']"
 
     attr_accessor :response, :setting, :data_to_find
 
@@ -12,15 +12,17 @@ module RucomServices
       self.response = {}
       self.response[:errors] = []
       #self.rucom_page = page || PAGE_URL  
-      self.setting = {}   
+      self.setting = nil
       self.data_to_find = data #{rol_name: 'Barequero', id_type: 'CEDULA', id_number: '15535725'}
     end
 
     def call
+      binding.pry
       validates_has_required_params_to_execute_service
       setting_service(@data_to_find)
-      raise "Error load settings from rucom_service.cfg.yml file" unless @setting[:success]
+      raise "Error load settings from rucom_service.cfg.yml file" unless @setting.success
       ##driver = Selenium::WebDriver.for :phantomjs
+      binding.pry
       html_page_data = navigate_and_get_results_from_searching(@setting.driver_instance) #(driver)
       formatted_data = formater_elements(html_page_data)
       ##virtus_model_name = (@data_to_find[:format].to_sym == :Barequero) ? "BarequeroResponse" : "TraderResponse"
@@ -31,10 +33,13 @@ module RucomServices
       #response[:success] = persist_data(data_formated)
       ##driver.quit
       @setting.driver_instance.quit
-      @response
+      #@response
+      self
     rescue Exception => e 
       puts "RucomService::Scraper.call error : #{e.message}" 
-      @response[:errors] << "RucomService::Scraper.call error : #{e.message}"
+      @response[:errors] << "RucomService::Scraper.call: #{e.message}"
+      #@response
+      self
     end  
 
     def setting_service(data_to_find)
@@ -42,9 +47,11 @@ module RucomServices
     end  
 
     def navigate_and_get_results_from_searching(driver)
+      binding.pry
       driver.navigate.to @setting.page_url ##PAGE_URL
       display_and_select_options_fields(driver)
-      button = driver.find_element(:id, 'form:consultar')
+      ##button = driver.find_element(:id, 'form:consultar')
+      button = driver.find_element(:id, @setting.clic_button_id)
       button.click      
       sleep(10)
       #pp driver.page_source
@@ -52,18 +59,16 @@ module RucomServices
       pp html_results.class
       table_content = 
         html_results.xpath("//*[@id='#{@setting.table_body_id}']").children.css('tr > td')
-      #pp table_content.each_with_object([]) {|td, obj| obj << td.text }      
     end
 
     def formater_elements(html_page_data)
-      options = {data: html_page_data, format: @data_to_find[:format].to_sym }
+      options = {data: html_page_data, format: @setting.response_class.to_sym }
       RucomServices::Formater.new.call(options)
     end
 
     def convert_to_virtus_model(formatted_data, virtus_model_name)
       binding.pry
       "RucomServices::#{virtus_model_name}".constantize.new(formatted_data)
-      1
     end
 
     def validate_data(virtus_model, virtus_model_name)
@@ -75,32 +80,47 @@ module RucomServices
     private
 
     def validates_has_required_params_to_execute_service
-      raise 'You must to provide a rol name option' if @data_to_find[:rol_name].blank?
-      raise 'You must to provide a Id type identification option' if @data_to_find[:id_type].blank?
-      raise 'You must to provide a Id number value' if @data_to_find[:id_number].blank?
+      @response[:errors]  << 'You must to provide a rol name option' if @data_to_find[:rol_name].blank?
+      @response[:errors]  << 'You must to provide a Id type identification option' if @data_to_find[:id_type].blank?
+      @response[:errors]  << 'You must to provide a Id number value' if @data_to_find[:id_number].blank?
+      msg = 'validates_has_required_params_to_execute_service: Error with sent input data from view'
+      raise msg if @response[:errors].count > 0
     end
 
     def display_and_select_options_fields(browser)
-      @data_to_find.values.each_with_index do |val, index|        
-        display_hidden_element(browser, HIDDEN_ELEMENTS_ID["step_#{index}".to_sym]) if index < 2
-        xpath_element = (index < 2) ? "//*[@data-label='#{val}']" : "//*[@id='form:numId']"
-        element = browser.find_element(:xpath => xpath_element)
-        if SELECT_OPTIONS.include?(val)
+      @data_to_find.values.each_with_index do |val, i|   
+        binding.pry     
+        display_hidden_element(browser, @setting.hidden_elements_id["step_#{i}"]) if i < 2
+        id = @setting.hidden_elements_id["step_#{i}"] if i > 1
+        xpath_element = (i < 2) ? "//*[@data-label='#{val}']" : "//*[@id='#{id}']"
+       
+        raise "can't find the element" \
+         "by xpath: #{xpath_element}" unless element = browser.find_element(:xpath => xpath_element)
+
+        rol_name = @setting.response[:send_data][:rol_name].downcase
+
+        if @setting.instance_eval("send(:#{rol_name})").include?(val)
           if element.displayed? 
             element.click
             sleep(2)
           else
-            puts "Error: Element hidden or can not do click" 
+            raise "Error: Element hidden or can not do clic #{element}" 
           end  
         else
           element.send_keys("#{val}")
         end  
-      end  
+      end
+    rescue Exception => e    
+      #@response[:errors] << "display_and_select_options_fields: #{e.message}"
+      raise "display_and_select_options_fields: #{e.message}"
     end
 
     def display_hidden_element(browser, id)
-        js_script = "$(\"[id='#{id}']\").css('display', 'block')"
-        browser.execute_script(js_script)
-    end
+      js_script = "$(\"[id='#{id}']\").css('display', 'block')"
+      browser.execute_script(js_script) 
+      true
+    rescue Exception => e
+      raise "display_hidden_element: Error executing script js_script: #{js_script}" 
+    end  
   end
 end
--- spec/lib/rucom_services/scraper_spec.rb
+++ spec/lib/rucom_services/scraper_spec.rb
@@ -1,20 +1,43 @@
 require 'spec_helper'
 
 describe RucomServices::Scraper, type: :service do
+  subject(:rss) { RucomServices::Scraper.new }
+
   before :each do
     @data = {rol_name: 'Barequero', id_type: 'CEDULA', id_number: '15535725'}
   end
 
-  it '#initialize' do
-    rss = RucomServices::Scraper.new(@data)
-    rss.call
+  xit '#initialize' do
+    rs_scraper = RucomServices::Scraper.new(@data)
+    rs_scraper.call
   end
 
-  it 'Validates the params from request has values' do
+  xit 'Validates the params from request has values' do
     @data = {}
-    rss = RucomServices::Scraper.new
     expect(rss.data_to_find).to eq({})
   end  
+
+  context "#call" do
+    context "When didn't send one of the parameters riquered from the view" do
+      it "raises and error indicating which parameter didn't send it" do
+        msg = 'RucomService::Scraper.call: ' \
+          'validates_has_required_params_to_execute_service: Error with sent input data from view'
+        expect(rss.call.response[:errors].count).to be > 0
+        expect(rss.call.response[:errors]).to include(msg)
+      end
+    end
+
+    context "When can not load the settings" do
+      xit "raises an error" do
+        msg = "RucomService::Scraper.call: Error load settings from rucom_service.cfg.yml file"
+        scraper = rss.call(@data)
+        #if scraper.setting.success
+          expect(rss.call.response[:errors].count).to be( 1)
+          expect(rss.call.response[:errors]).to include(msg)
+        #end
+      end  
+    end  
+  end
 
 end  
 
