73da38f67fd310d44f88bb6781182af17462f26a
Paulo Carmona <pcarmonaz@hotmail.com>
2016-07-28 15:17:06 -0500

    Apply rubocop to the Formater Service and its tests

--- lib/rucom_services/formater.rb
+++ lib/rucom_services/formater.rb
@@ -1,41 +1,42 @@
 #  RucomServices::Formater Module Allows to give a format to  returns data from the Rucom page search.
 module RucomServices
+  # This class set the format to the sent data
   class Formater
-    FIELDS_TRADERS = %i(rucom_number name minerals state)
-    FIELDS_AUTORIZED_PROV = %i(name minerals localization)
+    FIELDS_TRADERS = %i(rucom_number name minerals state).freeze
+    FIELDS_AUTORIZED_PROV = %i(name minerals localization).freeze
 
     attr_accessor :response
 
     def initialize
       self.response = {}
-      self.response[:errors] = []
-    end  
+      @response[:errors] = []
+    end
 
-    def call(options ={})      
+    def call(options = {})
       if options[:data] && options[:format]
-        @data = options[:data] 
+        @data = options[:data]
         format!(assign_fields(options))
-      else  
-        @response[:errors] << 
-          "call: No was send data as parameter when invoqued the method." unless options[:data]
-        @response[:errors] << 
-          "call: No was send the format Key when invoqued the method." unless options[:format]
+      else
+        @response[:errors] << 'call: No was send data as parameter when invoqued the method.' unless options[:data]
+        @response[:errors] << 'call: No was send the format Key when invoqued the method.' unless options[:format]
         @response
-      #fields = (options[:format] == :trader) ? FIELDS_TRADERS : FIELDS_AUTORIZED_PROV
       end
-    end  
-    
+    rescue StandardError => e
+      @response[:errors] << "call: Error #{e.message}"
+    end
+
     def assign_fields(options)
       if options[:format] == :trader_response
         FIELDS_TRADERS
       elsif options[:format] == :autorized_provider_response
         FIELDS_AUTORIZED_PROV
       else
-        store_and_raise_error("assign_fields: format option doesn't match: #{options[:format]}")
+        store_and_raise_error("assign_fields: format option doesn't match: #{options[:format]}", false)
+        nil
       end
     end
-    
-    def format!(field_names)  
+
+    def format!(field_names)
       store_and_raise_error("format!: field_names parameter can't be empty") if field_names.blank?
       field_names.each_with_index do |key, index|
         @response[key] = @data[index].content
@@ -43,9 +44,9 @@ module RucomServices
       @response
     end
 
-    def store_and_raise_error(str_message)
+    def store_and_raise_error(str_message, do_raise = true)
       @response[:errors] << str_message
-      raise str_message
+      raise str_message if do_raise
     end
   end
-end  
+end
--- spec/lib/rucom_services/formater_spec.rb
+++ spec/lib/rucom_services/formater_spec.rb
@@ -1,37 +1,37 @@
 require 'spec_helper'
 
 describe RucomServices::Formater, type: :service do
-  subject(:rs_formater) {RucomServices::Formater.new}
+  subject(:rs_formater) { RucomServices::Formater.new }
 
   it '#initialize' do
-    #rss = RucomServices::Formater.new
+    # rss = RucomServices::Formater.new
     expect(rs_formater.response.class).to eq(Hash)
     expect(rs_formater.response[:errors]).to eq([])
   end
-  
+
   context '#call' do
-    context "when sends autorized_provider_response key as format value " do
-      it "returns its respective virtus model" do
+    context 'when sends autorized_provider_response key as format value ' do
+      it 'returns its respective virtus model' do
         str_html = '<tr><td role="gridcell"><span style="white-space:normal">AMADO  MARULANDA </span></td>
                       <td role="gridcell"><span style="white-space:normal">ORO</span></td>
                       <td role="gridcell"><span style="white-space:normal">CORDOBA - PUERTO LIBERTADOR</span></td>
                     </tr>'
         response = {
-          name: "AMADO  MARULANDA ", 
-          minerals: "ORO", 
-          localization: "CORDOBA - PUERTO LIBERTADOR",
+          name: 'AMADO  MARULANDA ',
+          minerals: 'ORO',
+          localization: 'CORDOBA - PUERTO LIBERTADOR',
           errors: []
         }
 
         tds_results_html = Nokogiri::HTML(str_html).children.css('tr > td')
         options = { data: tds_results_html, format: :autorized_provider_response }
-        
-        expect(rs_formater.call(options)).to eq(response)
-      end 
-    end 
 
-    context "when sends trader_response key as format value " do
-      it "returns its respective virtus model" do
+        expect(rs_formater.call(options)).to eq(response)
+      end
+    end
+
+    context 'when sends trader_response key as format value ' do
+      it 'returns its respective virtus model' do
         str_html = '<tr data-ri="56" class="ui-widget-content ui-datatable-even" role="row">
                     <td role="gridcell"><span style="white-space:normal">RUCOM-20131218132</span></td>
                     <td role="gridcell"><span style="white-space:normal">   FUNDICIÓN RAMIREZ ZONA FRANCA S.A.S.</span></td>
@@ -39,10 +39,10 @@ describe RucomServices::Formater, type: :service do
                     <td role="gridcell"><span style="white-space:normal">Certificado</span></td>
                   </tr>'
         response = {
-          rucom_number: "RUCOM-20131218132",
-          name: "   FUNDICIÓN RAMIREZ ZONA FRANCA S.A.S.", 
-          minerals: "MINERALES DE ORO Y SUS CONCENTRADOS", 
-          state: "Certificado",
+          rucom_number: 'RUCOM-20131218132',
+          name: '   FUNDICIÓN RAMIREZ ZONA FRANCA S.A.S.',
+          minerals: 'MINERALES DE ORO Y SUS CONCENTRADOS',
+          state: 'Certificado',
           errors: []
         }
 
@@ -53,54 +53,54 @@ describe RucomServices::Formater, type: :service do
       end
     end
 
-    context "When no send data or/and format key" do
-      it "returns errors inside reponse[errors] array" do
+    context 'When no send data or/and format key' do
+      it 'returns errors inside reponse[errors] array' do
         str_html = '<tr>
                       <td role="gridcell"><span style="white-space:normal">AMADO  MARULANDA </span></td>
                       <td role="gridcell"><span style="white-space:normal">ORO</span></td>
                       <td role="gridcell"><span style="white-space:normal">CORDOBA - PUERTO LIBERTADOR</span></td>
                     </tr>'
         options = { data: nil, format: :autorized_provider_response }
-        error_msg = "call: No was send data as parameter when invoqued the method."
+        error_msg = 'call: No was send data as parameter when invoqued the method.'
         tds_results_html = Nokogiri::HTML(str_html).children.css('tr > td')
         expect(rs_formater.call(options)[:errors]).to include(error_msg)
 
         options = { data: tds_results_html, format: nil }
-        error_msg = "call: No was send the format Key when invoqued the method."
+        error_msg = 'call: No was send the format Key when invoqued the method.'
         expect(rs_formater.call(options)[:errors]).to include(error_msg)
 
         options = {}
         error2_msg = error_msg
-        error_msg = "call: No was send data as parameter when invoqued the method."
+        error_msg = 'call: No was send data as parameter when invoqued the method.'
         expect(rs_formater.call(options)[:errors]).to include(error_msg, error2_msg)
-      end  
+      end
     end
   end
 
-  context "#assign_fields" do
-    context "When format key value is autorized_provider_response" do
-      it "returns the fields of the autorized_provider_response" do
+  context '#assign_fields' do
+    context 'When format key value is autorized_provider_response' do
+      it 'returns the fields of the autorized_provider_response' do
         fields_autorized_prov = %i(name minerals localization)
-        options = {data: nil, format: :autorized_provider_response}
+        options = { data: nil, format: :autorized_provider_response }
 
         expect(rs_formater.assign_fields(options)).to eq(fields_autorized_prov)
       end
     end
 
-    context "When format key value is trader_response" do
-      it "returns the fields of the trader_response" do
+    context 'When format key value is trader_response' do
+      it 'returns the fields of the trader_response' do
         fields_trader = %i(rucom_number name minerals state)
-        options = {data: nil, format: :trader_response}
+        options = { data: nil, format: :trader_response }
 
         expect(rs_formater.assign_fields(options)).to eq(fields_trader)
       end
     end
 
-    context "When format key value is another key" do
-      it "returns nil as response" do
-        options = {data: nil, format: :other_thing}
-
+    context 'When format key value is another key' do
+      it 'raises an error and add the error inside errors response array' do
+        options = { data: nil, format: :other_thing }
         expect(rs_formater.assign_fields(options)).to eq(nil)
+        expect(rs_formater.response[:errors]).to include(/assign_fields: format option doesn't match\:/)
       end
     end
   end
