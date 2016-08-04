10da6ba80c03aea25d4ddb6e5930428ed38c7563
Santiago lopez <santilopez114@gmail.com>
2016-08-04 18:08:06 -0500

    fix errors of indenting

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,8 +1,7 @@
 ActiveAdmin.register User do
-
   menu priority: 6, label: 'Usuarios'
 
-  permit_params :id, :email, :office_id, :password, :password_confirmation, :external, :rucom, profile_attributes: [ :first_name, :last_name , :document_number, :phone_number , :address, :rut_file, :photo_file, :mining_authorization_file, :legal_representative, :nit_number, :city_id, :user_id ]
+  permit_params :id, :email, :office_id, :password, :password_confirmation, :external, :rucom, profile_attributes: [:first_name, :last_name, :document_number, :phone_number, :address, :rut_file, :photo_file, :mining_authorization_file, :id_document_file, :legal_representative, :nit_number, :city_id, :user_id]
 
   # overwrite controller update to perform upgrade Rucom.
   controller do
@@ -10,18 +9,18 @@ ActiveAdmin.register User do
       if params[:user][:rucom].blank?
         super
       elsif params[:user][:rucom].present?
-          user = User.find(params[:id])
-          rucom_id = params[:user].delete(:rucom)
-          if user.rucom.present?
-            user.rucom.update_attributes(rucomeable_id: nil)
-            rucom = Rucom.find_by_id(rucom_id)
-            rucom.update_attributes(rucomeable: user)
-            super
-          else
-            rucom = Rucom.find_by_id(rucom_id)
-            rucom.update_attributes(rucomeable: user)
-            super
-          end
+        user = User.find(params[:id])
+        rucom_id = params[:user].delete(:rucom)
+        if user.rucom.present?
+          user.rucom.update_attributes(rucomeable_id: nil)
+          rucom = Rucom.find_by_id(rucom_id)
+          rucom.update_attributes(rucomeable: user)
+          super
+        else
+          rucom = Rucom.find_by_id(rucom_id)
+          rucom.update_attributes(rucomeable: user)
+          super
+        end
       end
     end
   end
@@ -35,8 +34,8 @@ ActiveAdmin.register User do
         new_hash = permitted_params[:user].except(:rucom)
         new_hash[:personal_rucom] = Rucom.find(rucom_id)
         user = User.new(new_hash)
-        #this line temporal
-        #TODO: remove this line and implemeted a dropdawn to select the user role this will be done when the registration state machine have been implemented
+        # this line temporal
+        # TODO: remove this line and implemeted a dropdawn to select the user role this will be done when the registration state machine have been implemented
         user.roles << Role.find_by(name: 'authorized_producer')
         user.save!
         redirect_to admin_users_path
@@ -44,7 +43,7 @@ ActiveAdmin.register User do
     end
   end
 
-  #new and edit
+  # new and edit
   form do |f|
     if f.object.errors.size >= 1
       f.inputs 'Errors' do
@@ -52,13 +51,13 @@ ActiveAdmin.register User do
       end
     end
     f.inputs 'User' do
-      if params[:action] == "edit"
-         columns "Este Usuario Cuenta Actualmente Con #{user.office ? 'Rucom de empresa' : 'Rucom personal'}: #{user.rucom.name} - Numero: #{user.rucom.num_rucom}"
+      if params[:action] == 'edit'
+        columns "Este Usuario Cuenta Actualmente Con #{user.office ? 'Rucom de empresa' : 'Rucom personal'}: #{user.rucom.name} - Numero: #{user.rucom.num_rucom}"
       end
       f.input :email
-      f.input :office, label: 'office', :collection => Office.all.map{ |o| ["#{o.company.name if o.company} - #{o.name}", o.id]}
+      f.input :office, label: 'office', :collection => Office.all.map { |o| ["#{o.company.name if o.company} - #{o.name}", o.id] }
       unless user.has_office?
-        #TDDO: Encontrar la manera de que el desplegable seleccione  el rucom asociado al usuario que se esta editando. Tener en cuenta que la relacion User con Rucom es polimorfica.
+        # TDDO: Encontrar la manera de que el desplegable seleccione  el rucom asociado al usuario que se esta editando. Tener en cuenta que la relacion User con Rucom es polimorfica.
         f.input :rucom, label: 'Rucom', :collection => Rucom.all.map{ |rucom| [rucom.name, rucom.id]}
       end
       # f.input :rucom, label: 'Rucom', :collection => Rucom.all.map{["#{object.rucom ? "#{object.rucom.num_rucom }" : "#{object.rucom.num_rucom }"}", object.rucom.rucomeable_id]}
@@ -70,6 +69,7 @@ ActiveAdmin.register User do
        f.input :password_confirmation, label: 'password_confirmation'
       end
     end
+
     f.inputs 'User Details' do
       f.has_many :profile do |p|
         p.input :first_name, label: 'Nombre'
@@ -77,9 +77,10 @@ ActiveAdmin.register User do
         p.input :document_number, label: 'Numero de documento'
         p.input :phone_number, label: 'Numero telefonico'
         p.input :address, label: 'Direccion'
-        p.input :photo_file, :as => :file , label: 'Foto Usuario' , :hint => image_tag(p.object.photo_file.try(:thumb).try(:url))
+        p.input :photo_file, :as => :file, label: 'Foto Usuario', :hint => image_tag(p.object.photo_file.try(:thumb).try(:url))
         p.input :rut_file, :as => :file, label: 'PDF Rut'
         p.input :mining_authorization_file, :as => :file, label: 'PDF registro minero'
+        p.input :id_document_file, :as => :file, label: 'PDF Document_id'
         p.input :legal_representative, label: 'Representante Legal'
         p.input :nit_number, label: 'nit_number'
         p.input :city, label: 'Ciudad'
@@ -88,7 +89,7 @@ ActiveAdmin.register User do
     f.actions
   end
 
-  #index
+  # index
   index do
     selectable_column
     id_column
@@ -108,15 +109,15 @@ ActiveAdmin.register User do
     actions
   end
 
-  #filters
+  # filters
   filter :email
   filter :profile_first_name, :as => :string
   filter :profile_document_number, :as => :string
   filter :profile_phone_number, :as => :string
   filter :external
 
-  #form partial: 'form'
-  #show
+  # form partial: 'form'
+  # show
   show do
     attributes_table do
       row :email
@@ -147,6 +148,6 @@ ActiveAdmin.register User do
         end
       end
     end
-    #active_admin_comments
+    # active_admin_comments
   end
 end
