5933eebf828b8dd64ea8dfbd52099ecfff98ac08
Diego Gomez <diego.f.gomez.pardo@gmail.com>
2016-08-08 15:42:59 -0500

    add settings model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160805215754) do
+ActiveRecord::Schema.define(version: 20160808203611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -235,6 +235,12 @@ ActiveRecord::Schema.define(version: 20160805215754) do
 
   add_index "sales", ["buyer_id"], name: "index_sales_on_buyer_id", using: :btree
   add_index "sales", ["inventory_id"], name: "index_sales_on_inventory_id", using: :btree
+
+  create_table "settings", force: true do |t|
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "sold_batches", force: true do |t|
     t.float    "grams_picked"
