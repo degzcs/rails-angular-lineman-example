cd0c069ed342ac898f9407377333cc25f00dd801
Santiago lopez <santilopez114@gmail.com>
2016-08-10 14:12:46 -0500

    new configuration but still does not work

--- spec/features/admin/users_spec.rb
+++ spec/features/admin/users_spec.rb
@@ -1,11 +1,17 @@
+require 'spec_helper'
+
 describe 'the signin process' do
   it 'signs me in' do
     visit '/admin/login'
-    within("#login") do
-      fill_in 'Email', :with => 'soporte@trazoro.co'
-      fill_in 'Password', :with => 'A7l(?/]03tal9-%g4'
+    within('#login') do
+      fill_in 'Email*', :with => 'soporte@trazoro.co'
+      fill_in 'Password*', :with => 'A7l(?/]03tal9-%g4'
     end
     click_button 'Login'
-    expect(page).to have_content 'Signed in successfully.'
+    save_and_open_page
+    binding.pry
+    if page.current_url == 'http://127.0.0.1/admin'
+      expect(page).to have_content 'Signed in successfully.'
+    end
   end
 end
