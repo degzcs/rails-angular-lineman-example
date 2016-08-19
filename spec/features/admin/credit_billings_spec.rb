e9e9141b53757406d95bbd844acbd5b72ada8356
Santiago lopez <santilopez114@gmail.com>
2016-08-19 10:36:47 -0500

    refactoring code

--- spec/features/admin/credit_billings_spec.rb
+++ spec/features/admin/credit_billings_spec.rb
@@ -36,20 +36,29 @@ describe 'all test the credit_billings view', :js do
       click_on('Facturar')
     end
     expect(page).to have_content 'Datos de Transaccion'
-    # click_link('Enviar Factura a usuario')
+    click_link('Enviar Factura a usuario')
     # click_link('Cancelar Envio de factura')
-    # click_on('OK')
-    # expect(page).to have_content 'El correo ha sido enviado a dcm@trazoro.co satisfactoriamente'
+    page.execute_script 'window.confirm = function () { return true }'
+    expect(page).to have_content "El correo ha sido enviado a #{credit_billing.user.email} satisfactoriamente"
   end
 
   it 'action mark as paid out test' do
+    expected_response = {
+      payment_date: 'Fri, 19 Aug 2016 09:00:00 UTC +00:00',
+      payment_flag: true
+    }
     credit_billing = create(:credit_billing, payment_flag: false, payment_date: nil, total_amount: 1000000)
     visit '/admin/credit_billings'
     within("#credit_billing_#{credit_billing.id}") do
       click_on('Actions')
       click_on('Marcar como pagado')
     end
-    # date field in capybara?
     expect(page).to have_content 'Ingrese la fecha en la que el usuario pago esta factura'
+    fill_in 'credit_billing_payment_date', with: '2016/08/19, 09:00 AM'
+    click_button('Marcar como pagado')
+    page.execute_script 'window.confirm = function () { return true }'
+    expect(credit_billing.reload.payment_date).to eq expected_response[:payment_date]
+    expect(credit_billing.reload.payment_flag).to eq expected_response[:payment_flag]
+    expect(page).to have_content 'La factura fue marcada como pagada satisfactoriamente'
   end
 end
