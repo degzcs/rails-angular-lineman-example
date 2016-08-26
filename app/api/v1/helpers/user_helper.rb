e1bb18c5e84479b77a0553e591ac0dfbe2e55e2d
Paulo Carmona <pcarmonaz@hotmail.com>
2016-08-26 11:10:02 -0500

    Fix  EndPoint Authorized Provider and its test

--- app/api/v1/helpers/user_helper.rb
+++ app/api/v1/helpers/user_helper.rb
@@ -23,6 +23,26 @@ module V1
           new_params [:profile_data] = params.slice(*Profile.columns_hash.except('id').keys)
           new_params
         end
+
+        # It is used in Authorized providers endPoint
+        # # @param params [ Hash ] => with files from the frontend
+        # @return [ Hash ]
+        def format_params_files(params)
+          params.tap do
+            trazoro = false
+            files = params['profile'].slice('files')['files']
+            photo_file = files.select{|file| file['filename'] =~ /photo_file/}.first
+            document_number_file = files.select{|file| file['filename'] =~ /document_number_file/}.first
+            mining_register_file = files.select{|file| file['filename'] =~ /mining_register_file/}.last
+
+            add_files_to_profile = {
+              :photo_file => photo_file,
+              :id_document_file => document_number_file,
+              :mining_authorization_file => mining_register_file
+            }
+            params['profile'].except!('files').merge!(add_files_to_profile)
+          end
+        end
       end
     end
   end
