cdb67a67be714b8328552979c2e52c2a840baecd
Santiago lopez <santilopez114@gmail.com>
2016-08-30 14:55:39 -0500

    remove origin cretificate_file

--- app/api/v1/helpers/purchase_helper.rb
+++ app/api/v1/helpers/purchase_helper.rb
@@ -7,17 +7,19 @@ module V1
           params.tap do
             trazoro = false
             files = params[:purchase].slice(:files)[:files]
-            origin_certificate_file = files.reject{|file| file['filename'] =~ /seller_picture/}.first
+            # origin_certificate_file = files.reject{|file| file['filename'] =~ /seller_picture/}.first
             seller_picture = files.select{|file| file['filename'] =~ /seller_picture/}.first
             signature_picture = files.select{|file| file['filename'] =~ /seller_picture/}.last
 
-            if origin_certificate_file.nil? && params[:purchase][:sale_id].present?
-              origin_certificate_file = Sale.find( params[:purchase][:sale_id] ).origin_certificate_file
-              trazoro = true
-            end
+            # TODO: this implementattion has to be changed in order to work with the new trazoro purchase requiriments. This cause problems with the normal purchases.
+
+            # if origin_certificate_file.nil? && params[:purchase][:sale_id].present?
+            #   origin_certificate_file = Sale.find( params[:purchase][:sale_id] ).origin_certificate_file
+            #   trazoro = true
+            # end
             params[:gold_batch][:extra_info] = JSON.parse(params[:gold_batch][:extra_info])
 
-            params[:purchase].except!(:files).merge!(origin_certificate_file: origin_certificate_file, seller_picture: seller_picture, signature_picture: signature_picture, trazoro: trazoro)
+            params[:purchase].except!(:files).merge!(seller_picture: seller_picture, signature_picture: signature_picture, trazoro: trazoro)
           end
         end
 
