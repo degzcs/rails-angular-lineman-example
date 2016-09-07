688ef7e9a3a02f5b6d8268b2fc54fa7a75395a3e
Santiago lopez <santilopez114@gmail.com>
2016-09-07 16:20:19 -0500

    fix errors in creating of office

--- app/admin/office.rb
+++ app/admin/office.rb
@@ -12,31 +12,26 @@
 ActiveAdmin.register Office do
   menu priority: 5, label: 'Sucursales'
 
-  permit_params :name, :company_id
+  permit_params :name, :city_id, :address, :company_id
   belongs_to :company
 
   controller do
     # This code is evaluated within the controller class
-
     def office
       # Instance method
       Office.find(params[:id])
     end
   end
 
-  sidebar "Lista de usuarios", only: [:show, :edit] do
-
+  sidebar 'Lista de usuarios', only: [:show, :edit] do
     ul do
       office.users.each do |user|
         li link_to user.profile.first_name, admin_user_path(user)
       end
     end
-
     # a link_to "Crear Usuario", new_admin_office_user_path(office), class: "create_child_button"
     # a link_to "Ver lista con detalles", admin_office_users_path(office)
-
   end
-
 
   index do
     selectable_column
@@ -49,13 +44,13 @@ ActiveAdmin.register Office do
   filter :name
   filter :company
 
-
   form do |f|
-    f.inputs "Informacion de sucursal" do
-      f.input :name , label: "Nombre"
-      f.input :company, label: "Compañia"
+    f.inputs 'Informacion de sucursal' do
+      f.input :name, label: 'Nombre'
+      f.input :company, label: 'Compañia'
+      f.input :city, label: 'Ciudad'
+      f.input :address, label: 'Direccion'
     end
     f.actions
   end
-
 end
