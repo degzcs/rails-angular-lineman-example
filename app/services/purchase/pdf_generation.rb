2f06ea0d1c29b47050f4508c571caec244cf98a3
Paulo Carmona <pcarmonaz@hotmail.com>
2016-09-12 22:27:02 -0500

    PdfGeneration Refactoring and clean up the rubocop offenses

--- app/services/purchase/pdf_generation.rb
+++ app/services/purchase/pdf_generation.rb
@@ -1,54 +1,63 @@
-class Purchase::PdfGeneration
+module Purchase
+  # Service PdfGeneration
+  class PdfGeneration
+    attr_accessor :order_presenter, :draw_pdf_service_class
 
-  attr_accessor :order_presenter, :draw_pdf_service_class
+    def initialize
+      @response = {}
+      @response[:errors] = []
+    end
 
-  def initialize(options={})
-    @response = {}
-    @response[:errors] = []
-  end
-
-  # @return [ Hash ] with the success or errors
-  def call(options={})
-    raise "You must to provide a purchase_order param" if options[:purchase_order].blank?
-    raise "You must to provide a draw_pdf_service param" if options[:draw_pdf_service].blank?
-    raise "You must to provide a document_type param" if options[:document_type].blank?
-    @order_presenter = OrderPresenter.new(options[:purchase_order], nil)
-    @draw_pdf_service_class = options[:draw_pdf_service]
-    timestamp = options[:timestamp] || Time.now.to_i
-    document_type = options[:document_type]
-
-    temporal_folder_path = "#{ Rails.root }/tmp/purchases/#{timestamp}"
-    # document_type = 'equivalent_document' # TODO: update this when it is made a invoice instead equiv_doc.
-    temporal_file_path = "#{temporal_folder_path}/#{document_type}.pdf"
-
-    if create_temporal_folder(temporal_folder_path)
-      order_presenter = generate_file_for(@order_presenter, temporal_file_path, document_type, options)
+    # @return [ Hash ] with the success or errors
+    def call(options = {})
+      validate_sent_options(options)
+      @order_presenter = OrderPresenter.new(options[:purchase_order], nil)
+      @draw_pdf_service_class = options[:draw_pdf_service]
+      file = define_temporal_dir_and_file(options)
+      raise 'The folder was not creted!' unless create_temporal_folder(file.path_folder)
+      order_presenter = generate_file_for(@order_presenter, file.path_file, file.name, options)
       @response[:success] = order_presenter.save!
       @response[:errors] << order_presenter.errors.full_messages
-    else
-      raise 'The folder was not creted!'
+      @response
     end
-    @response
-  end
 
-  private
+    # @return [ Struct Object ] with the :path_folder, :path_file, :name as document_type
+    def define_temporal_dir_and_file(options)
+      timestamp = options[:timestamp] || Time.now.to_i
+      document_type = options[:document_type]
 
-  def generate_file_for(order_presenter, temporal_file_path, document_type, options)
-    draw_pdf_service = draw_pdf_service_class.new
-    response = draw_pdf_service.call(options.merge(order_presenter: order_presenter))
-    file = draw_pdf_service.file
-    file.render_file(temporal_file_path)
-    order_presenter.documents.build(
-      file: File.open(temporal_file_path),
-      type: document_type,
+      temporal_folder_path = "#{Rails.root}/tmp/purchases/#{timestamp}"
+      # document_type = 'equivalent_document' # TODO: update this when it is made a invoice instead equiv_doc.
+      temporal_file_path = "#{temporal_folder_path}/#{document_type}.pdf"
+      tmp = Struct.new(:path_folder, :path_file, :name)
+      tmp.new(temporal_folder_path, temporal_file_path, document_type)
+    end
+
+    def validate_sent_options(options)
+      raise 'You must to provide a purchase_order param' if options[:purchase_order].blank?
+      raise 'You must to provide a draw_pdf_service param' if options[:draw_pdf_service].blank?
+      raise 'You must to provide a document_type param' if options[:document_type].blank?
+    end
+
+    private
+
+    def generate_file_for(order_presenter, temporal_file_path, document_type, options)
+      draw_pdf_service = draw_pdf_service_class.new
+      draw_pdf_service.call(options.merge(order_presenter: order_presenter))
+      file = draw_pdf_service.file
+      file.render_file(temporal_file_path)
+      order_presenter.documents.build(
+        file: File.open(temporal_file_path),
+        type: document_type
       )
-  end
+    end
 
-  # @param folder_path [ String ] where the files will be saved temporarily
-  # @return [ Boolean ]
-  def create_temporal_folder(folder_path)
-    system <<-COMMAND
-      mkdir -p #{ folder_path } && cd #{ folder_path }
-    COMMAND
+    # @param folder_path [ String ] where the files will be saved temporarily
+    # @return [ Boolean ]
+    def create_temporal_folder(folder_path)
+      system <<-COMMAND
+        mkdir -p #{folder_path} && cd #{folder_path}
+      COMMAND
+    end
   end
 end
--- spec/services/purchase/pdf_generation_spec.rb
+++ spec/services/purchase/pdf_generation_spec.rb
@@ -1,25 +1,22 @@
 require 'spec_helper'
 
 describe Purchase::PdfGeneration do
-  let(:buyer){ create :user, :with_company }
-  let(:gold_batch){ create(:gold_batch, fine_grams: 10, grade: 999, extra_info: { grams: 10 }) }
-  let(:purchase_order){ create :purchase, buyer: buyer, gold_batch: gold_batch }
-  let(:service){ Purchase::PdfGeneration.new }
+  let(:buyer) { create :user, :with_company }
+  let(:gold_batch) { create(:gold_batch, fine_grams: 10, grade: 999, extra_info: { grams: 10 }) }
+  let(:purchase_order) { create :purchase, buyer: buyer, gold_batch: gold_batch, performer: buyer }
+  let(:service) { Purchase::PdfGeneration.new }
 
   context 'check process' do
-
     before :each do
-      signature_picture_path = "#{ Rails.root }/spec/support/images/signature.png"
-      @signature_picture =  Rack::Test::UploadedFile.new(signature_picture_path, "image/jpeg")
+      signature_picture_path = "#{Rails.root}/spec/support/images/signature.png"
+      @signature_picture = Rack::Test::UploadedFile.new(signature_picture_path, 'image/jpeg')
     end
 
     it 'should to create a pdf file with the correct information' do
-      response = service.call(
-                        purchase_order: purchase_order,
-                        signature_picture: @signature_picture,
-                        draw_pdf_service: ::Purchase::ProofOfPurchase::DrawPDF,
-                        document_type: 'equivalent_document',
-                        )
+      response = service.call(purchase_order: purchase_order,
+                              signature_picture: @signature_picture,
+                              draw_pdf_service: ::Purchase::ProofOfPurchase::DrawPDF,
+                              document_type: 'equivalent_document')
       expect(response[:success]).to be true
       # TODO: update this when it is made a invoice instead equiv_doc.
       expect(purchase_order.reload.proof_of_purchase.file.path).to match(/equivalent_document.pdf/)
