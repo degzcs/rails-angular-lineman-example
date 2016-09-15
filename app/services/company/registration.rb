830777312bb4e51d9083faa4e3c307b6434631d1
Santiago lopez <santilopez114@gmail.com>
2016-09-15 16:54:43 -0500

    change code for update a company already created for the scraper service

--- app/services/company/registration.rb
+++ app/services/company/registration.rb
@@ -1,5 +1,4 @@
 class Company::Registration
-
   attr_accessor :legal_representative
   attr_accessor :company
   attr_accessor :response
@@ -10,12 +9,11 @@ class Company::Registration
   end
 
   def call(options={})
-    raise 'You must to provide a legal_representative_data option' if options[:legal_representative_data].blank?
     raise 'You must to provide a company_data option' if options[:company_data].blank?
-    raise 'You must to provide a rucom option' if options[:rucom].blank?
+    raise 'You must to provide a legal_representative_data option' if options[:legal_representative_data].blank?
     # @response[:errors] = "Error, don't got the options call"
     @legal_representative = create_legal_representative_from(options[:legal_representative_data])
-    @company = create_company_from(options[:company_data], legal_representative, options[:rucom])
+    @company = update_company_from(options[:company_data], legal_representative)
     # associate legal representative with the main company office
     legal_representative.update_column(:office_id, company.main_office.id) if legal_representative.persisted? && company.persisted?
     @response
@@ -34,12 +32,14 @@ class Company::Registration
   # @param company_data [ Hash ]
   # @param legal_representative [ User ]
   # @return [ Company ]
-  def create_company_from(company_data, legal_representative, rucom)
-  company = Company.create(
-      company_data.merge(legal_representative: legal_representative, rucom: rucom)
-    )
-    @response[:success] = company.save
-    @response[:errors] << company.errors.full_messages
-    company
+  def update_company_from(company_data, legal_representative)
+    company_id = company_data[:id] || company_data['id']
+    company = Company.find(company_id)
+    if company.update_attributes(company_data.merge(legal_representative: legal_representative))
+      @response[:success] = true
+      company
+    else
+      @response[:errors] << company.errors.full_messages
+    end
   end
 end
