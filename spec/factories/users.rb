b7956dc4b59e86b198c4b39ca02775a7c675f3d2
Paulo Carmona <pcarmonaz@hotmail.com>
2016-09-16 16:50:32 -0500

    Add Logic to audit the user creator

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -74,6 +74,7 @@ FactoryGirl.define do
       end
       before :create do |user, e|
         user.personal_rucom = create(:rucom, provider_type: e.provider_type)
+        user.performer_id = 1
       end
     end
 
