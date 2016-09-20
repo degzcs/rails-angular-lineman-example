4f44ed4ae7976e92e2328a801ad54147ddfacdd6
Santiago lopez <santilopez114@gmail.com>
2016-09-20 09:23:38 -0500

    add with_profile

--- spec/services/user_reset_password_spec.rb
+++ spec/services/user_reset_password_spec.rb
@@ -1,16 +1,11 @@
 require 'spec_helper'
 
 describe UserResetPassword do
-
-  let(:user){ create(:user, :with_personal_rucom) }
-
-  subject(:user_reset_password){ UserResetPassword.new user}
-
+  let(:user){ create(:user, :with_profile, :with_personal_rucom) }
+  subject(:user_reset_password){ UserResetPassword.new user }
   context 'new password' do
-
     it 'should to send a email to reset the user password' do
-       expect(user_reset_password.process!).to be_a_kind_of Mail::Message
+      expect(user_reset_password.process!).to be_a_kind_of Mail::Message
     end
-
   end
 end
