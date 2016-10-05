81d362bda680d6b5add3e9a71e0fa4cb928eab18
luis murillo <lumx2@hotmail.com>
2016-10-05 14:13:59 -0500

    fix service shipment draw pdf

--- app/services/shipment/draw_pdf.rb
+++ app/services/shipment/draw_pdf.rb
@@ -1,17 +1,28 @@
+require 'barby'
+require 'barby/barcode/ean_13'
+require 'barby/outputter/prawn_outputter'
 # Draw application shipment authorization and/or
 class Shipment::DrawPdf < Prawn::Document
-  attr_accessor :base_file
+  attr_accessor :base_file, :order_presenter
 
   def initialize(options = {})
-    super(skip_page_creation: true)
+    super(skip_page_creation: false)
+    @response = {}
+    @response[:errors] = []
   end
 
   def call(options = {})
-  raise "You must to provide a order_presenter option" if options[:order_presenter].blank?
-    order_presenter = options[:order_presenter]
-    @base_file = options[:base_file] || File.open(File.join(Rails.root, 'vendor','pdfs','solicitud_de_autorizacion_embarque_yo_registros_previos.pdf'))
-    draw_file!(order_presenter)
-    self
+  raise "You must to provide a order_presenter option" if options[:order].blank?
+    @order_presenter = OrderPresenter.new(options[:order], nil)
+    @base_file = options[:base_file] || File.open(File.join(Rails.root, 'vendor','pdfs','solicitud_de_autorizacion_embarque_yo_registros_previos.pdf'))    
+    begin
+      draw_file!(order_presenter)
+      @response[:success] = true
+    rescue => exception
+      @response[:success] = false
+      @response[:errors] << exception.message
+    end
+    @response
   end
 
   def file
@@ -25,5 +36,7 @@ class Shipment::DrawPdf < Prawn::Document
     # move_down 60
     move_cursor_to 1200
     text_box "Prueba de que dibuja"
+    ##### Buyer #######
+    
   end
 end
