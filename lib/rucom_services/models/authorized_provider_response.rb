986089f1814e0b4b907d19b6dedae4019afdc726
Santiago Lopez <santilopez114@gmail.com>
2016-10-12 17:00:40 -0500

    add validation for trade gold on AuthorizedProviderResponse class

--- lib/rucom_services/models/authorized_provider_response.rb
+++ lib/rucom_services/models/authorized_provider_response.rb
@@ -2,11 +2,9 @@ module RucomServices
   module Models
     # The  Autorized providers are: Chatarreros and Barequeros
     class AuthorizedProviderResponse
-      include ActiveModel::Model
+      include ActiveModel::Validations
       include Virtus.model(nullify_blank: true)
       include ::RucomServices::Formater
-
-      # before_save :format_values!
 
       attr_accessor :rucom
       attribute :name, String
@@ -14,14 +12,14 @@ module RucomServices
       attribute :location, String
       attribute :original_name, String
       attribute :provider_type, String
-      attribute :errors, Array
 
       # Validations
-      validates :name, presence: true
-      validates :minerals, presence: true
-      validates :original_name, presence: true
-      validates :provider_type, presence: true
-      validates :location, presence: true
+      validates_presence_of :name
+      validates_presence_of :minerals
+      validates_presence_of :original_name
+      validates_presence_of :provider_type
+      validates_presence_of :location
+      validate :can_buy_gold?
 
       #
       # Instance Methods
@@ -46,24 +44,25 @@ module RucomServices
 
       private
 
+      def can_buy_gold?
+        errors.add(:minerals, 'Este productor no puede comercializar ORO') unless self.minerals.include?('ORO' || 'oro')
+      end
+
       def persist!
         # Status: values = 'Activo' | 'Inactivo'
         # This field dosen't return from rucom it will be set as:
         # 'Activo' when appears in the rucom database otherwise
         fields_mapping = {
           name: name,
-          minerals: minerals,
+          minerals: minerals.first, # TODO: update rucom#minerals field type to Array
           location: location,
           status: 'Activo',
           original_name: original_name,
           provider_type: provider_type
         }
-        if fields_mapping[:minerals].include?('ORO' || 'oro')
-          r = format_values!(fields_mapping)
-          Rucom.create!(r)
-        else
-          raise 'Este productor no puede comercializar ORO'
-        end
+
+          # r = format_values!(fields_mapping)
+          Rucom.create!(fields_mapping)
       end
     end
   end
--- spec/lib/rucom_services/models/authorized_provider_response_spec.rb
+++ spec/lib/rucom_services/models/authorized_provider_response_spec.rb
@@ -16,11 +16,41 @@ describe RucomServices::Models::AuthorizedProviderResponse do
 
     it 'should create a valid virtus model' do
       virtus_model = virtus_model_class.new(@parser_result)
-      expect(virtus_model.name).to eq 'AMADO'
-      expect(virtus_model.minerals).to eq ['ORO']
-      expect(virtus_model.location).to eq 'COLOMBIA'
-      expect(virtus_model.original_name).to eq 'AMADO'
-      expect(virtus_model.provider_type).to eq 'BAREQUERO'
+      expect(virtus_model.name).to eq @parser_result[:value_1]
+      expect(virtus_model.minerals).to eq @parser_result[:value_2]
+      expect(virtus_model.location).to eq @parser_result[:value_3]
+      expect(virtus_model.original_name).to eq @parser_result[:value_4]
+      expect(virtus_model.provider_type).to eq @parser_result[:value_5]
+    end
+
+    it 'should persist the current virtus model into rucoms table' do
+      virtus_model = virtus_model_class.new(@parser_result)
+      virtus_model.save
+      rucom = virtus_model.rucom
+      expect(rucom.persisted?).to eq true
+      expect(rucom.name).to eq @parser_result[:value_1]
+      expect(rucom.minerals).to eq @parser_result[:value_2].first # TODO: update rucom#minerals field type to Array
+      expect(rucom.location).to eq @parser_result[:value_3]
+      expect(rucom.original_name).to eq @parser_result[:value_4]
+      expect(rucom.provider_type).to eq @parser_result[:value_5]
+    end
+  end
+
+  context 'validations' do
+    before :each do
+     @parser_result = {
+        value_1: 'AMADO',
+        value_2: ['PLATA'],
+        value_3: 'COLOMBIA',
+        value_4: 'AMADO',
+        value_5: 'BAREQUERO'
+      }
+    end
+
+    it 'should validate BAREQUERO is able to trade GOLD' do
+      virtus_model = virtus_model_class.new(@parser_result)
+      virtus_model.valid?
+      expect(virtus_model.errors.full_messages.first).to eq 'Minerals Este productor no puede comercializar ORO'
     end
   end
 end
