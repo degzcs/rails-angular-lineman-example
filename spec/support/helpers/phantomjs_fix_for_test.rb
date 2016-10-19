447f17b20caf4ab65a4b754913eceefa2923b9cc
Diego Gomez <diego.f.gomez.pardo@gmail.com>
2016-10-19 15:55:51 -0500

    fix phantomjs for testing env: update spec_helper config and add correct cassettes

--- spec/support/helpers/phantomjs_fix_for_test.rb
+++ spec/support/helpers/phantomjs_fix_for_test.rb
@@ -1,4 +1,8 @@
 if Rails.env.test?
+  # NOTE: This fix the issues with VCR and its cassettes. The thing is that the gem is very especial,
+  # I meant smart one (for real environments ), it trys to to find a free port in case that the 8910 port is busy.
+  # This avoid that VCR handle in a correct war evry request to the RUCOM page on test env (on dev and prod is OK)
+  # So the idea with this fix is avoid that dynamic assigment.
   module Selenium
     module WebDriver
       module PhantomJS
