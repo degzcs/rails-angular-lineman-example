310028b66ea5d10cc28369fb5447c533c0c91dde
luis murillo <lumx2@hotmail.com>
2016-10-20 17:35:31 -0500

    remove city key

--- spec/services/shipment/draw_pdf_spec.rb
+++ spec/services/shipment/draw_pdf_spec.rb
@@ -5,7 +5,7 @@ describe Shipment::DrawPdf do
     }
     let(:courier) { create :courier }
     let(:rucom) { create :rucom, rucom_number: '7767899877' }
-    let(:buyer) { create :user, :with_profile, :with_company, :with_trader_role, city: City.find_by(name: 'MEDELLIN'), name: 'Aquiles S.A', nit_number: '0987654321', rucom: rucom, first_name: 'nametest', last_name: 'last_nametest', document_number: '1234567890', city: City.find_by(name: 'MEDELLIN') }
+    let(:buyer) { create :user, :with_profile, :with_company, :with_trader_role, name: 'Aquiles S.A', nit_number: '0987654321', rucom: rucom, first_name: 'nametest', last_name: 'last_nametest', document_number: '1234567890', city: City.find_by(name: 'MEDELLIN') }
     let(:gold_batch) { create :gold_batch }
     let(:sale_order) { create :sale, seller: seller, buyer: buyer, gold_batch: gold_batch, courier: courier }
 
@@ -17,7 +17,6 @@ describe Shipment::DrawPdf do
       @order = create(:sale)
     end
     it 'should check the consistency of the document' do
-      binding.pry
       expected_hash = '2ebeaa2e22115cb5a6d84aad646f808e3aafe20f389ee73515becca13198d1d7'
       response = service.call(
         current_user: buyer,
