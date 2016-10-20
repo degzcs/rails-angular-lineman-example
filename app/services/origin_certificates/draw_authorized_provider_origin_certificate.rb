9c8a09431f41eebe08d6f9c9608e8a54b30369db
Diego Gomez <diego.f.gomez.pardo@gmail.com>
2016-10-20 13:06:35 -0500

    refactor code: split logic in several methods and save temporal images into a temporal folder

--- app/services/origin_certificates/draw_authorized_provider_origin_certificate.rb
+++ app/services/origin_certificates/draw_authorized_provider_origin_certificate.rb
@@ -107,11 +107,45 @@ class OriginCertificates::DrawAuthorizedProviderOriginCertificate < Prawn::Docum
     text_box buyer_presenter.rucom_number, :at => [350, cursor], :width => 300, :height => 15, :overflow => :shrink_to_fit
     move_cursor_to 100
     signature = signature_picture.is_a?(Hash) ? OpenStruct.new(signature_picture) : signature_picture
-    # binding.pry
-    signature_file = File.read(signature.tempfile.path)
-    File.write('signature_test.jpg', signature_file)
-    system "convert signature_test.jpg -fill none -fuzz 1% -draw 'matte 0,0 floodfill' -flop  -draw 'matte 0,0 floodfill' -flop signature_test.png"
+    signature_path = generate_signature_without_background(signature)
+    image(signature_path, :at => [400, cursor], :fit => [500,60])
+  end
 
-    image('signature_test.png', :at => [400, cursor], :fit => [500,60])
+  # @param signature [ OpenStruct ] or Tempfile obejct
+  # @return [ String ] with the image path converted, it means the image path without background.
+  def generate_signature_without_background(signature)
+    signature_file = File.read(signature.tempfile.path)
+    folder_config = define_temporal_dir_and_file
+    create_temporal_folder(folder_config.temporal_folder_path)
+    File.write(folder_config.income_image_path, signature_file)
+    was_convert = remove_background_for(folder_config.income_image_path, folder_config.outcome_image_path)
+    was_convert ? folder_config.outcome_image_path : raise('This image cannot be removed its background!!!')
+  end
+
+  # @param income_image_path [ String ]
+  # @param outcome_image_path [ String ]
+  # @return [ Boolean ] truen when the command runs perfectly and false in other cases.
+  def remove_background_for(income_image_path, outcome_image_path)
+    system "convert #{ income_image_path } -fill none -fuzz 1% -draw 'matte 0,0 floodfill' -flop  -draw 'matte 0,0 floodfill' -flop #{ outcome_image_path }"
+  end
+
+  # @param folder_path [ String ] where the files will be saved temporarily
+  # @return [ Boolean ]
+  def create_temporal_folder(folder_path)
+    system <<-COMMAND
+      mkdir -p #{folder_path} && cd #{folder_path}
+    COMMAND
+  end
+
+  # @params option [ Hash ]
+  # @return [ Struct Object ] with the :temporal_folder_path, :income_image_path, :outcome_image_path
+  def define_temporal_dir_and_file(options={})
+    timestamp = options[:timestamp] || Time.now.to_i
+    filename = options[:filename] || 'signature'
+    temporal_folder_path = "#{ Rails.root }/tmp/signatures/#{ timestamp }"
+    income_image_path = "#{ temporal_folder_path }/#{ filename }.jpg"
+    outcome_image_path = "#{ temporal_folder_path }/#{ filename }.png"
+    tmp = Struct.new(:temporal_folder_path, :income_image_path, :outcome_image_path)
+    tmp.new(temporal_folder_path, income_image_path, outcome_image_path)
   end
 end
