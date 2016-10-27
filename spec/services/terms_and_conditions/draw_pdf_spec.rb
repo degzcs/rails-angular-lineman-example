76860c81dd63ecffc803f6a52710dc5e4487a478
luis murillo <lumx2@hotmail.com>
2016-10-26 23:12:50 -0500

    instance the presenter in the spec

--- spec/services/terms_and_conditions/draw_pdf_spec.rb
+++ spec/services/terms_and_conditions/draw_pdf_spec.rb
@@ -5,14 +5,16 @@ describe TermsAndConditions::DrawPdf do
 
   context 'creation of the pdf in the service habeas_data_agreetment draw' do
     before :each do
-      @seller = create(:user, :with_profile, :with_personal_rucom, provider_type: 'Barequero')
-      @signature_picture_path = "#{Rails.root}/spec/support/images/signature.png"
+      seller = create(:user, :with_profile, :with_personal_rucom, provider_type: 'Barequero')
+      @seller_presenter = UserPresenter.new(seller, nil)
+      signature_picture_path = "#{Rails.root}/spec/support/images/signature.png"
+      @signature_picture = Rack::Test::UploadedFile.new(signature_picture_path, 'image/jpeg')
     end
     it 'should check the consistency of the document' do
-      expected_hash = 'f7d9c940556349de8983bfa1de1e4dc8316e24a5719a811c20a2418ded517c57'
+      expected_hash = '00948c31cb0b1420b45b2ae29617c2db4aefe8cfc7a42df5801fc8c158cc6a41'
       response = service.call(
-        authorized_provider_presenter: @seller,
-        signature_picture: @signature_picture_path
+        authorized_provider_presenter: @seller_presenter,
+        signature_picture: @signature_picture
       )
       system "mkdir -p #{ Rails.root }/tmp/terms_and_conditions"
       saved_file = service.file.render_file("#{ Rails.root }/tmp/terms_and_conditions/habeas_data_agreetment_test.pdf")
