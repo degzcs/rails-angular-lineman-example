776c751c742c2c64efcab1ac4f4476fbdbab5e66
Santiago Lopez <santilopez114@gmail.com>
2016-10-31 11:32:00 -0500

    add new module Micromachine user_states

---
+++ app/services/state_machines/user_states.rb
@@ -0,0 +1,60 @@
+module StateMachines
+  module UserStates
+    STATES = {
+      initialized: 'insertado desde rucom',
+      completed: 'Completado',
+      failure: 'Error'
+      # paused: ''
+    }
+
+    def self.included(base)
+      base.send 'before_save', :persist_status
+      base.extend(ClassMethods)
+    end
+
+    def status_label
+      registration_state && STATES[registration_state.to_sym]
+    end
+
+    def initialized?
+      (registration_state || status.state) == 'initialized'
+    end
+
+    def completed?
+      registration_state == 'completed'
+    end
+
+    def failure?
+      registration_state == 'failure'
+    end
+
+    def crash!
+      status.trigger(:crash)
+    end
+
+    def complete!
+      status.trigger!(:complete)
+    end
+
+    def status
+      @status ||= begin
+                    mcm = MicroMachine.new(registration_state || 'initialized')
+                    mcm.when(:crash, 'initialized' => 'failure')
+                    mcm.when(:complete, 'initialized' => 'completed')
+                    mcm
+                  end
+    end
+
+    module ClassMethods
+      def states_for_select
+        STATES.collect { |val| [val[1], val[0]] }
+      end
+    end
+
+    private
+
+    def persist_status
+      self.registration_state = status.state
+    end
+  end
+end
