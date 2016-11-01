cacdaf55463ce30e45be6dc495afb384df7b7e36
Santiago Lopez <santilopez114@gmail.com>
2016-10-31 21:28:04 -0500

    fix spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -267,7 +267,9 @@ describe  User, type: :model do
     it 'should synchronize a trader when it is created' do
       VCR.use_cassette('trader_alegra_sync_response') do
         user = create :user, :with_profile, :with_company, :with_trader_role
-        user.complete!
+        user.user_complete
+        user.save
+        user.alegra
         expect(user.completed?).to eq true
         expect(user.alegra_id).not_to eq nil
         expect(user.alegra_sync).to eq true
