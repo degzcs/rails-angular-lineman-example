7de9b690bb1fc9419813c7b4b5e3dda27e29c40d
Diego Gomez <diego.f.gomez.pardo@gmail.com>
2016-11-03 16:55:43 -0500

    annotate classes

--- app/models/user_setting.rb
+++ app/models/user_setting.rb
@@ -3,7 +3,7 @@
 # Table name: user_settings
 #
 #  id              :integer          not null, primary key
-#  state           :boolean
+#  state           :boolean          default(FALSE)
 #  alegra_token    :string(255)
 #  profile_id      :integer
 #  created_at      :datetime
