699120ae14c6168f173fb42136fcf2f6751356f4
Santiago Lopez <santilopez114@gmail.com>
2016-11-03 17:59:01 -0500

    finish specs for the model UserSetting

--- spec/models/user_setting_spec.rb
+++ spec/models/user_setting_spec.rb
@@ -13,6 +13,30 @@
 
 require 'spec_helper'
 
-RSpec.describe UserSetting, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe UserSetting, type: :model do
+  context 'factory text' do
+    let(:user_setting) { create :user_setting }
+    it 'should to test the profile factory' do
+      expect(user_setting).to be_valid
+      expect(user_setting.profile.present?).to eq true
+    end
+  end
+
+  context 'associations with available_trazoro_services' do
+    it { should have_and_belong_to_many :available_trazoro_services }
+  end
+
+  context 'should show error of Validations' do
+    it 'error Validations fine_gram_value is not a number' do
+      setting = build :user_setting, fine_gram_value: 'text'
+      setting.save
+      expect(setting.errors.full_messages).to include('Fine gram value is not a number')
+    end
+
+    it 'should validate unique association between user setting and available trazoro services' do
+      user_setting = create(:user_setting, :with_available_trazoro_service)
+      available_trazoro_service = user_setting.available_trazoro_services.last
+      expect { user_setting.available_trazoro_services << available_trazoro_service }.to raise_error(ActiveRecord::RecordNotUnique)
+    end
+  end
 end
