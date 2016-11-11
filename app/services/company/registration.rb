cafd03e00e636522bd2992c5cbc4a1f855fd3897
Santiago Lopez <santilopez114@gmail.com>
2016-11-11 14:20:03 -0500

    service refactoring and add state of legal_representative

--- app/services/company/registration.rb
+++ app/services/company/registration.rb
@@ -11,12 +11,16 @@ class Company::Registration
   def call(options={})
     raise 'You must to provide a company_data option' if options[:company_data].blank?
     raise 'You must to provide a legal_representative_data option' if options[:legal_representative_data].blank?
-    # @response[:errors] = "Error, don't got the options call"
     options.deep_symbolize_keys!
     @company = update_company_from(options[:company_data])
     user_email = options[:legal_representative_data][:email]
     legal_representative_data = options[:legal_representative_data]
     validates_user_exist(user_email, legal_representative_data)
+    @response[:success] = @response[:errors].blank?
+    @response
+  rescue StandardError => e
+    @response[:errors] << e
+    @response[:success] = false
     @response
   end
 
@@ -26,15 +30,11 @@ class Company::Registration
     if User.find_by(email: user_email)
       update_legal_representative_from(legal_representative_data)
     else
-      begin
-        user_data = legal_representative_data.merge(office: company.main_office)
-        @legal_representative = create_legal_representative_from(user_data)
-        # associate legal representative with the main company office
-        company.update_attributes(legal_representative: legal_representative)
-        company.complete || company.pause # NOTE: if the company registration state doesn't apply to completed state it is going to stay on draft state
-      rescue Exception => e
-        @response[:errors] << e
-      end
+      user_data = legal_representative_data.merge(office: company.main_office)
+      @legal_representative = create_legal_representative_from(user_data)
+      # associate legal representative with the main company office
+      company.update_attributes(legal_representative: legal_representative)
+      company.complete || company.pause # NOTE: if the company registration state doesn't apply to completed state it is going to stay on draft state
     end
   end
 
@@ -43,17 +43,12 @@ class Company::Registration
   def create_legal_representative_from(user_data)
     user = User.new(user_data.except(:profile_attributes))
     user.roles << Role.find_by(name: 'trader')
-    if user.save!
-      @response[:success] = true
-    else
-      @response[:errors] << user.errors.full_messages
-    end
+    user.save!
     user.build_profile(user_data[:profile_attributes])
     user.profile.legal_representative = true
     user_setting = UserSetting.new(fine_gram_value: 0.0)
     user.profile.setting = user_setting
-    @response[:success] = user.save
-    @response[:errors] << user.errors.full_messages
+    user.user_complete?
     user
   end
 
@@ -62,12 +57,8 @@ class Company::Registration
   def update_legal_representative_from(user_data)
     user_id = user_data[:id]
     user = User.find(user_id)
-    if user.update_attributes(user_data.except(:profile_attributes)) && user.profile.update_attributes(user_data[:profile_attributes])
-      @response[:success] = true
-      user
-    else
-      @response[:errors] << user.errors.full_messages
-    end
+    user.update_attributes(user_data.except(:profile_attributes)) && user.profile.update_attributes(user_data[:profile_attributes])
+    user
   end
 
   # @param company_data [ Hash ]
@@ -76,12 +67,8 @@ class Company::Registration
   def update_company_from(company_data)
     company_id = company_data[:id]
     company = Company.find(company_id)
-    if company.update_attributes(company_data)
-      company.complete || company.pause # NOTE: if the company registration state doesn't apply to completed state it is going to stay on draft state
-      @response[:success] = true
-      company
-    else
-      @response[:errors] << company.errors.full_messages
-    end
+    company.update_attributes(company_data)
+    company.complete || company.pause # NOTE: if the company registration state doesn't apply to completed state it is going to stay on draft state
+    company
   end
 end
