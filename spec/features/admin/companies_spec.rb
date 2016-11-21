02dd60f1bdb90dab8ef66da51e6eac0ea7f5b181
Santiago Lopez <santilopez114@gmail.com>
2016-11-21 16:26:01 -0500

    refactor specs and add news validations in the creation or upgrade of company

--- spec/features/admin/companies_spec.rb
+++ spec/features/admin/companies_spec.rb
@@ -49,7 +49,6 @@ describe 'all test the companies view', :js do
       mining_authorization_file: "#{ Rails.root }/spec/support/pdfs/mining_register_file.pdf",
       id_document_file: "#{ Rails.root }/spec/support/pdfs/document_number_file.pdf",
       nit_number: '12345676',
-      city_id: City.last.id,
       # company fields
       name: 'Company test',
       email_company: 'compañia@gmail.com',
@@ -140,25 +139,65 @@ describe 'all test the companies view', :js do
   end
 
   it 'Company draft state' do
-    company = FactoryGirl.create(:company, name: nil, email: nil, phone_number: nil, chamber_of_commerce_file: nil, mining_register_file: nil, rut_file: nil, nit_number: '1110001')
+    company = create(:company, name: nil, email: nil, phone_number: nil, chamber_of_commerce_file: nil, mining_register_file: nil, rut_file: nil, nit_number: '1110001')
+    company.legal_representative.destroy!
+    company.update_column(:legal_representative_id, nil)
+    company.offices[0].destroy!
+    company.reload
     expected_response = {
-      address: 'calle 45 # 32',
+      # user fields
+      email: 'lam.ehbotas@thecompany.com',
+      password: 'this_is_my_password',
+      password_confirmation: 'this_is_my_password',
+      # profile fields
+      first_name: 'Lam',
+      last_name: 'Ehbotas',
+      document_number: '987654321',
+      phone_number: '3004445566',
+      available_credits: 0,
+      address: 'Street fake 123',
+      rut_file: "#{ Rails.root }/spec/support/pdfs/rut_file.pdf",
+      photo_file: "#{ Rails.root }/spec/support/images/photo_file.png",
+      mining_authorization_file: "#{ Rails.root }/spec/support/pdfs/mining_register_file.pdf",
+      id_document_file: "#{ Rails.root }/spec/support/pdfs/document_number_file.pdf",
+      nit_number: '12345676',
+      # company fields
+      address_company: 'calle 45 # 32',
       city: City.first.name,
-      phone_number: '3445565'
+      phone_number_company: '3445565'
     }
     visit '/admin/companies'
     within("#company_#{ company.id }") do
       click_link('Edit')
     end
     select(expected_response[:city], from: 'company_city_id')
-    fill_in 'company_phone_number', with: expected_response[:phone_number]
-    fill_in 'company_address', with: expected_response[:address]
+    fill_in 'company_phone_number', with: expected_response[:phone_number_company]
+    fill_in 'company_address', with: expected_response[:address_company]
+    fill_in 'company_legal_representative_attributes_email', with: expected_response[:email]
+    fill_in 'company_legal_representative_attributes_password', with: '12345678'
+    fill_in 'company_legal_representative_attributes_password_confirmation', with: '12345678'
+    fill_in 'company_legal_representative_attributes_profile_attributes_first_name', with: expected_response[:first_name]
+    fill_in 'company_legal_representative_attributes_profile_attributes_last_name', with: expected_response[:last_name]
+    fill_in 'company_legal_representative_attributes_profile_attributes_document_number', with: expected_response[:document_number]
+    fill_in 'company_legal_representative_attributes_profile_attributes_phone_number', with: expected_response[:phone_number]
+    fill_in 'company_legal_representative_attributes_profile_attributes_address', with: expected_response[:address]
+    attach_file('company_legal_representative_attributes_profile_attributes_photo_file', expected_response[:photo_file])
+    attach_file('company_legal_representative_attributes_profile_attributes_rut_file', expected_response[:rut_file])
+    attach_file('company_legal_representative_attributes_profile_attributes_mining_authorization_file', expected_response[:mining_authorization_file])
+    attach_file('company_legal_representative_attributes_profile_attributes_id_document_file', expected_response[:id_document_file])
+    check('company_legal_representative_attributes_profile_attributes_legal_representative')
+    fill_in 'company_legal_representative_attributes_profile_attributes_nit_number', with: expected_response[:nit_number]
+    select('MEDELLIN', from: 'company_legal_representative_attributes_profile_attributes_city_id')
+
     click_button('Update Company')
-    expect(company.reload.address).to eq expected_response[:address]
+
+    expect(company.reload.address).to eq expected_response[:address_company]
     expect(company.city.name).to eq expected_response[:city]
-    expect(company.phone_number).to eq expected_response[:phone_number]
+    expect(company.phone_number).to eq expected_response[:phone_number_company]
     expect(company.offices.present?).to eq true
     expect(company.rucom.present?).to eq true
+    expect(company.legal_representative.present?).to eq true
+    expect(company.legal_representative.completed?).to eq true
     expect(company.draft?).to eq true
   end
 
@@ -170,6 +209,7 @@ describe 'all test the companies view', :js do
     }
     company = create(:company, name: nil, email: nil, phone_number: nil)
     company.pause
+    company.legal_representative.complete!
     expect(company.draft?).to eq true
     visit '/admin/companies'
     within("#company_#{ company.id }") do
@@ -186,6 +226,7 @@ describe 'all test the companies view', :js do
     expect(company.offices.present?).to eq true
     expect(company.rucom.present?).to eq true
     expect(company.legal_representative.present?).to eq true
+    expect(company.legal_representative.completed?).to eq true
   end
 
   it 'When the company can not market oro' do
