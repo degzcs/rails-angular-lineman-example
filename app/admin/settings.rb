1c9d002a77ca2659a846b7425f619ca7a1270543
Paulo Carmona <pcarmonaz@hotmail.com>
2016-11-22 08:06:21 -0500

    Add last_transaction_sequence field to active admin settings

--- app/admin/settings.rb
+++ app/admin/settings.rb
@@ -1,16 +1,18 @@
 ActiveAdmin.register Settings do
   menu priority: 11, label: 'Configuracion'
-  permit_params :data, :monthly_threshold, :fine_gram_value, :vat_percentage, :fixed_sale_agreetment, :habeas_data_agreetment
+  permit_params :last_transaction_sequence, :data, :monthly_threshold, :fine_gram_value, :vat_percentage, :fixed_sale_agreetment, :habeas_data_agreetment
 
   index do
     selectable_column
     id_column
+    column :last_transaction_sequence
     column :data
     actions
   end
 
   form do |f|
     f.inputs 'Configuracion' do
+      f.input :last_transaction_sequence, label: 'Sequencia de la última transacción'
       f.input :monthly_threshold, label: 'Limite mensual por barequero/chatarrero (gramos)'
       f.input :vat_percentage, label: 'Porcentaje IVA (%)'
       f.input :fine_gram_value, label: 'Valor gramo fino (COP)'
