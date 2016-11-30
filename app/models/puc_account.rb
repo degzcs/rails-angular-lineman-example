06b0bb232dde178cb5aa99f23e211ce8d8633bec
Paulo Carmona <pcarmonaz@hotmail.com>
2016-11-30 07:21:31 -0500

    Add attribute validations to PucAccount model

--- app/models/puc_account.rb
+++ app/models/puc_account.rb
@@ -1,2 +1,18 @@
+# PucAccount class to load the tax module Puc
 class PucAccount < ActiveRecord::Base
+  validates :code, presence: true
+  validates :name, presence: true
+  validates :debit, presence: true
+  validates :credit, presence: true
+  validates :account_nature, presence: true
+
+  ACCOUNT_NATURES = {
+    D: 'Dédito',
+    C: 'Crédito'
+  }.freeze
+
+  OPERATIONS = {
+    :'+' => 'Suma',
+    :'-' => 'Resta'
+  }.freeze
 end
