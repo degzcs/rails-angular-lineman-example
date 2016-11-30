d964b53271c1ca80754f15855034b62fc73576f9
Paulo Carmona <pcarmonaz@hotmail.com>
2016-11-30 08:11:44 -0500

    Add validations and associations between transaction_movement and puc_account models

--- app/models/puc_account.rb
+++ app/models/puc_account.rb
@@ -1,6 +1,7 @@
 # PucAccount class to load the tax module Puc
 class PucAccount < ActiveRecord::Base
   has_many :taxes
+  has_many :transaction_movements
 
   validates :code, presence: true
   validates :name, presence: true
--- app/models/transaction_movement.rb
+++ app/models/transaction_movement.rb
@@ -1,3 +1,8 @@
+# TransactionMovement class allows to list the puc_accounts by block names group
 class TransactionMovement < ActiveRecord::Base
   belongs_to :puc_account
+
+  validates :puc_account_id, presence: true
+  validates :type, presence: true
+  validates :block_name, presence: true
 end
