7651e046209aa77f2eb94840648240ab19a3d2e3
Paulo Carmona <pcarmonaz@hotmail.com>
2016-11-30 08:02:41 -0500

    Add validations and associations between apply_tax and tax models

--- app/models/apply_tax.rb
+++ app/models/apply_tax.rb
@@ -1,3 +1,8 @@
+# ApplyTax class storage the rules to apply the taxes
 class ApplyTax < ActiveRecord::Base
   belongs_to :tax
+
+  validates :tax_id, presence: true
+  validates :seller_regime, presence: true
+  validates :buyer_regime, presence: true
 end
--- app/models/tax.rb
+++ app/models/tax.rb
@@ -1,6 +1,7 @@
 # Tax class storage the taxes for the puc accounts
 class Tax < ActiveRecord::Base
   belongs_to :puc_account
+  has_many :apply_taxes
 
   validates :puc_account_id, presence: true
   validates :name, presence: true
