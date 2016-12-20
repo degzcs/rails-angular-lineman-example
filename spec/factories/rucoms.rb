7a60ce0314d3dd5d79aa4c14c46e1a81ec02f9e5
Diego Gomez <diego.f.gomez.pardo@gmail.com>
2016-12-20 10:35:52 -0500

    refactor synchronize spec and create clean helper methods

--- spec/factories/rucoms.rb
+++ spec/factories/rucoms.rb
@@ -21,12 +21,12 @@ FactoryGirl.define do
     rucom_number { "RUCOM-20131220411-#{ Rucom.count }" }
     name { "Empresa falsa # #{ Rucom.count }" }
     original_name { "Empresa falsa # #{ Rucom.count }" }
-    minerals { 'ORO' }
+    minerals { 'ORO' } # Plata or Platino
     location { '' }
-    status { ['certificado', 'rechazado', 'vencido', 'En trámite, pendiente de evaluación'].sample }
+    status { ['certificado', 'rechazado', 'vencido', 'en_tramite, pendiente_de_evaluacion'].sample }
     # Provider Types
     # 1) chatarrero, 2) barequero, 3) titular minero, 4) beneficiario de area de reserva especial, 5) solicitante de legalizacion, 6) subcontrato de formalizaion
     # TODO: Every name here have to be formatted with parameterize and underscore methods
-    provider_type { ['Titular', 'Solicitante Legalización De Minería', 'Beneficiario Área Reserva Especial', 'Barequero', 'Chatarrero', 'Casa de Compraventa'].sample }
+    provider_type { ['titular', 'solicitante_legalizacion_de_mineria', 'beneficiario_area_reserva_especial', 'barequero', 'chatarrero', 'casa_de_compraventa'].sample }
   end
 end
