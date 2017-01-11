dec2d7d5cf4ce39b726fb3bbf942eb11e4906229
Paulo Carmona <pcarmonaz@hotmail.com>
2017-01-11 16:46:33 -0500

    Fix factories from user_settings

--- spec/factories/user_settings.rb
+++ spec/factories/user_settings.rb
@@ -20,7 +20,6 @@ FactoryGirl.define do
     fine_gram_value 0.0
     last_transaction_sequence 0
     regime_type { 'RS' }
-    activity_code {[]}
     scope_of_operation ''
     organization_type ''
     self_holding_agent ''
